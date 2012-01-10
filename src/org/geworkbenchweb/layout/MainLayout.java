1ba9b80365c4629202625de3fcfcbf3159064b15
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-01-10 11:26:05 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@8679 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/layout/MainLayout.java
+++ src/org/geworkbenchweb/layout/MainLayout.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
 import org.geworkbenchweb.GeworkbenchApplication;
 import org.geworkbenchweb.dataset.DataSetUpload;
 import org.geworkbenchweb.pojos.DataSet;
@@ -12,6 +13,7 @@ import org.vaadin.appfoundation.authentication.SessionHandler;
 import org.vaadin.appfoundation.authentication.data.User;
 import org.vaadin.appfoundation.persistence.facade.FacadeFactory;
 
+import com.vaadin.data.Container;
 import com.vaadin.data.Item;
 import com.vaadin.data.Property;
 import com.vaadin.data.Property.ValueChangeEvent;
@@ -26,6 +28,7 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
 import com.vaadin.ui.Tree;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
@@ -66,7 +69,7 @@ public class MainLayout extends AbsoluteLayout {
 		setSizeFull();
         mainPanel.setSizeFull();
         mainPanel.setStyleName("small previews");
-        mainPanel.setSplitPosition(15);        
+        mainPanel.setSplitPosition(20);        
               
         tabs.setStyleName("opaque");
         tabs.setSizeFull();
@@ -112,74 +115,78 @@ public class MainLayout extends AbsoluteLayout {
 		
 		private static final long serialVersionUID = 4523693969296820932L;
 		
+		private Table markerTable;
+		
+		private Table arrayTable;
+		
 		AccordionPanels(boolean closable) {
 			
 			super();
-			for (int i=1; i<4; i++) {
-				
-				VerticalLayout l 	= 	new VerticalLayout();
-				l.setMargin(true);
-				Tab t = addTab(l);
-				if(i == 1) {
-					
-					t.setCaption("DataSets");
-					t.setStyleName("sidebar-menu");
-					
-					VerticalLayout dataSets = 	new VerticalLayout();
-					Button updateDataset 	= 	new Button("Upload DataSet", new ClickListener() {
+			VerticalLayout l 	= 	new VerticalLayout();
+			l.setMargin(true);
+			Tab t = addTab(l);
 
-						private static final long serialVersionUID = 658137872256766310L;
 
-						@Override
-						public void buttonClick(ClickEvent event) {
-							
-							dataWindow = new DataSetUpload();
-							app.getMainWindow().addWindow(dataWindow);
-							app.initView(getApplication().getMainWindow());
-						}
-						
-					});
-					
-					updateDataset.setStyleName("small");
-					updateDataset.setIcon(new ThemeResource("../runo/icons/16/document-add.png"));
-					
-					dataSets.addComponent(updateDataset);
-					dataSets.setComponentAlignment(updateDataset, Alignment.TOP_CENTER);
-					
-					dataTree = new Tree();
-					dataTree.areChildrenAllowed(true);
-					
-					dataTree.addContainerProperty("DataSet Name", String.class, "");
-					dataTree.setContainerDataSource(getDataContainer());
-					dataSets.addComponent(dataTree);
-					
-					dataTree.addActionHandler(this);
-					dataTree.addListener(this);
-			        dataTree.setImmediate(true);
-					
-					l.addComponent(dataSets);
-					l.setHeight("100%");
-				
+			t.setCaption("DataSets");
+			t.setStyleName("sidebar-menu");
+
+			VerticalLayout dataSets = 	new VerticalLayout();
+			Button updateDataset 	= 	new Button("Upload DataSet", new ClickListener() {
+
+				private static final long serialVersionUID = 658137872256766310L;
+
+				@Override
+				public void buttonClick(ClickEvent event) {
+
+					dataWindow = new DataSetUpload();
+					app.getMainWindow().addWindow(dataWindow);
+					app.initView(getApplication().getMainWindow());
 				}
-				
-				if(i == 2) {
-					
-					t.setCaption("Markers");
-					l.addComponent(new Label("Markers of the selected datasets if  available"));
-				
-				}
-				
-				if(i == 3) {
-					
-					t.setCaption("Arrays");
-					l.addComponent(new Label("Arrays of the selected datasets if available"));
-				
-				}
-				
-				t.setIcon(new ThemeResource("../runo/icons/16/folder.png"));
-				t.setClosable(closable);
+
+			});
+
+			updateDataset.setStyleName("small");
+			updateDataset.setIcon(new ThemeResource("../runo/icons/16/document-add.png"));
+
+			dataSets.addComponent(updateDataset);
+			dataSets.setComponentAlignment(updateDataset, Alignment.TOP_CENTER);
+
+			dataTree = new Tree();
+			dataTree.areChildrenAllowed(true);
+
+			dataTree.addContainerProperty("DataSet Name", String.class, "");
+			dataTree.setContainerDataSource(getDataContainer());
+			dataSets.addComponent(dataTree);
+
+			dataTree.addActionHandler(this);
+			dataTree.addListener(this);
+			dataTree.setImmediate(true);
+
+			l.addComponent(dataSets);
+			l.setHeight("100%");
+
+			t.setIcon(new ThemeResource("../runo/icons/16/folder.png"));
 			
-			}
+			markerTable 	= 	new Table();
+			markerTable.addStyleName("small striped");
+			markerTable.setSizeFull();
+			markerTable.setSelectable(true);
+			markerTable.setMultiSelect(true);
+			
+			Tab t1 = addTab(markerTable);
+			t1.setCaption("Makers");
+			t1.setIcon(new ThemeResource("../runo/icons/16/folder.png"));
+			
+			arrayTable 	= 	new Table();
+			arrayTable.addStyleName("small striped");
+			arrayTable.setSizeFull();
+			arrayTable.setSelectable(true);
+			arrayTable.setMultiSelect(true);
+			
+			Tab t2 = addTab(arrayTable);
+			t2.setCaption("Phenotypes");
+			t2.setIcon(new ThemeResource("../runo/icons/16/folder.png"));
+			
 		}
 
 		@Override
@@ -191,19 +198,127 @@ public class MainLayout extends AbsoluteLayout {
 		@Override
 		public void valueChange(ValueChangeEvent event) {
 			
-			if (event.getProperty().getValue() != null) {
+			try {
+			
+				if (event.getProperty().getValue().toString().contains("Analysis") && event.getProperty().getValue().toString().contains("SubSets")) {
 				
-				VisualPlugin tabSheet = new VisualPlugin((String) event.getProperty().getValue());
-				mainPanel.setSecondComponent(tabSheet);
-				mainPanel.requestRepaint();				
+						//TODO
 				
-			} else {
+				} else {
 				
-				//TODO:Handle it later
+					VisualPlugin tabSheet = new VisualPlugin((String) event.getProperty().getValue());
+					mainPanel.setSecondComponent(tabSheet);
+					
+					String dataPeru					= 	(String) event.getProperty().getValue();
+					String query 					= 	"Select p from DataSet as p where p.name=:name and p.owner=:owner";
+					Map<String, Object> parameters 	= 	new HashMap<String, Object>();
+					
+					parameters.put("name", dataPeru);
+					parameters.put("owner", user.getId());
+					
+					DataSet dataSet 				= 	FacadeFactory.getFacade().find(query, parameters);
+					
+					if(dataSet != null) {
+						
+						byte[] dataByte 			= 	dataSet.getData();
+						DSMicroarraySet maSet 		= 	(DSMicroarraySet) toObject(dataByte);
+
+						markerTable.setColumnHeaderMode(Table.COLUMN_HEADER_MODE_HIDDEN);
+						markerTable.setContainerDataSource(markerTableView(maSet));
+						
+						arrayTable.setColumnHeaderMode(Table.COLUMN_HEADER_MODE_HIDDEN);
+						arrayTable.setContainerDataSource(arrayTableView(maSet));
+						
+					} 
+					
+					mainPanel.requestRepaint();		
+				}
 				
+			} catch (NullPointerException e) {
+				
+				//TODO
 			}
 		}
 
+		/**
+		 * Method is used to populate Phenotype Panel
+		 * @param maSet
+		 * @return - Indexed container with array labels
+		 */
+		
+		private Container arrayTableView(DSMicroarraySet maSet) {
+			
+			IndexedContainer tableData 		= 	new IndexedContainer();
+
+				for(int k=0;k<maSet.size();k++) {
+					
+						Item item 					= 	tableData.addItem(k);
+						tableData.addContainerProperty("Labels", String.class, null);
+						item.getItemProperty("Labels").setValue(maSet.get(k));
+						 
+				}
+			
+			
+			return tableData;
+		}
+
+		/**
+		 * Method is used to populate Marker Panel
+		 * @param maSet
+		 * @return - Indexed container with marker labels
+		 */
+		
+		private IndexedContainer markerTableView(DSMicroarraySet maSet) {
+	
+			IndexedContainer tableData 		= 	new IndexedContainer();
+
+			for(int j=0; j<maSet.getMarkers().size();j++) {
+				
+				Item item 					= 	tableData.addItem(j);
+				
+				for(int k=0;k<=maSet.size();k++) {
+					
+					if(k == 0) {
+						
+						tableData.addContainerProperty("Labels", String.class, null);
+						item.getItemProperty("Labels").setValue(maSet.getMarkers().get(j));
+						
+					} 
+				}
+			}
+			return tableData;
+	
+		}
+
+
+
+
+		@SuppressWarnings("deprecation")
+		public Object toObject(byte[] bytes){ 
+			
+			Object object = null; 
+			
+			try{ 
+				
+				object = new java.io.ObjectInputStream(new 
+						java.io.ByteArrayInputStream(bytes)).readObject(); 
+			
+			}catch(java.io.IOException ioe){ 
+				
+				java.util.logging.Logger.global.log(java.util.logging.Level.SEVERE, 
+						ioe.getMessage()); 
+			
+			}catch(java.lang.ClassNotFoundException cnfe){ 
+				
+				java.util.logging.Logger.global.log(java.util.logging.Level.SEVERE, 
+						cnfe.getMessage()); 
+			
+			} 
+			
+			return object; 
+		
+		}
+		
 		@Override
 		public Action[] getActions(Object target, Object sender) {
 			
@@ -215,34 +330,40 @@ public class MainLayout extends AbsoluteLayout {
 		public void handleAction(Action action, Object sender, Object target) {
 			
 			if (action == ACTION_DELETE) {
-				
-				
+
 				String dataName 				=	(target.toString());
+
 				String query 					= 	"Select p from DataSet as p where p.name=:name and p.owner=:owner";
 				Map<String, Object> parameters 	= 	new HashMap<String, Object>();
-				
+
 				parameters.put("name", dataName);
 				parameters.put("owner", user.getId());
-				
+
 				DataSet dataSet 				= 	FacadeFactory.getFacade().find(query, parameters);
-				
+
+
 				if(dataSet != null) {
-					
+
 					FacadeFactory.getFacade().delete(dataSet);
+					dataTree.removeItem(target + " - Analysis");
+					dataTree.removeItem(target + " - SubSets");
 					dataTree.removeItem(target);
-				
-				} else {
 					
+
+				} else {
+
 					String querySub 					= 	"Select p from ResultSet as p where p.name=:name and p.owner=:owner";
 					Map<String, Object> params 			= 	new HashMap<String, Object>();
-					
+
 					params.put("name", dataName);
 					params.put("owner", user.getId());
 					ResultSet resultSet 				= 	FacadeFactory.getFacade().find(querySub, params);
 					FacadeFactory.getFacade().delete(resultSet);
 					dataTree.removeItem(target);
 					
+
 				}
+
 			}
 			
 		}
@@ -261,7 +382,12 @@ public class MainLayout extends AbsoluteLayout {
 			
 			String id = (String) data.get(i);
 		    dataSets.addItem(id);
-
+		    
+		    String analysisDone = id + " - Analysis";
+		    
+		    dataSets.addItem(analysisDone);
+		    dataSets.setChildrenAllowed(analysisDone, true);
+		    dataSets.setParent(analysisDone, id);
 		    
 		    Map<String, Object> params 	= 	new HashMap<String, Object>();
 		    params.put("owner", user.getId());
@@ -274,13 +400,19 @@ public class MainLayout extends AbsoluteLayout {
 		    	String subId = (String) results.get(j);
 		    	dataSets.addItem(subId);
 		    	dataSets.setChildrenAllowed(subId, false);
-		    	dataSets.setParent(subId, id);
+		    	dataSets.setParent(subId, analysisDone);
 		    	
 		    }
-		
+		    
+		    String subSets = id + " - SubSets";
+			dataSets.addItem(subSets);
+		    dataSets.setChildrenAllowed(subSets, true);
+		    dataSets.setParent(subSets, id);
+		 
 		}
 		
-		return dataSets;
+		
+	    return dataSets;
 	
 	}
 }
