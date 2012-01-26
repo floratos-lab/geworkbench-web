9af88a89636605e3b691922e33cc4594fe2b3d2d
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-01-26 22:07:14 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@8788 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbenchweb/pojos/SubSet.java
@@ -0,0 +1,64 @@
+package org.geworkbenchweb.pojos;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.vaadin.appfoundation.persistence.data.AbstractPojo;
+
+@Entity
+@Table(name="SubSet")
+public class SubSet extends AbstractPojo {
+
+	private static final long serialVersionUID = -2720207271844335675L;
+
+	private String name;
+	private String type;
+	private Long owner;
+	private Long parent;
+	private Set<?> positions;
+
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public Long getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Long owner) {
+		this.owner = owner;
+	}
+	
+	public Long getParent() {
+		return parent;
+	}
+
+	public void setParent(Long parent) {
+		this.parent = parent;
+	}
+	
+	public Set<?> getPositions() {
+		return positions;
+	}
+	
+	public void setPositions(Set<?> positions) {
+		this.positions = positions;
+	}
+	
+	
+}
