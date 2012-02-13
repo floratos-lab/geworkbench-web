63130fa306ab6c1639decc416842c24fde50288a
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-02-13 21:55:03 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@8877 8b29bb6e-589a-435a-9a12-3ab907be3462

---
+++ src/org/geworkbenchweb/analysis/hierarchicalclustering/DendrogramTab.java
@@ -0,0 +1,13 @@
+package org.geworkbenchweb.analysis.hierarchicalclustering;
+
+import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
+
+import com.vaadin.ui.VerticalLayout;
+
+public class DendrogramTab extends VerticalLayout{
+
+	public DendrogramTab(Object dataSet, DSMicroarraySet maSet) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
