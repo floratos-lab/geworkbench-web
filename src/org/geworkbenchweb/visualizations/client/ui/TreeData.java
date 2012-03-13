8de45ab566dcbcc5c94b2933998c3fb236afe852
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-03-13 21:49:55 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@9155 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/visualizations/client/ui/TreeData.java
+++ src/org/geworkbenchweb/visualizations/client/ui/TreeData.java
@@ -17,39 +17,40 @@ public final class TreeData {
     
     public static JavaScriptObject data(String treeString) {
 
-		PVDomNode rootObject 		=	PVDomNode.create(new Object(), "root",  0);
-		
-		PVDomNode parentNode		=	rootObject;  
-		Stack<PVDomNode> prevNode 	=	new Stack<PVDomNode>();
+    	Stack<PVDomNode> prevNode 	=	new Stack<PVDomNode>();
+		PVDomNode rootObject = null; 
+		PVDomNode parentNode = null;
 		
 		for(int i = 0; i<treeString.length(); i++ ) {
 
-			if(treeString.charAt(i) == '(' ) {
-				
-				PVDomNode childNode  	= 	PVDomNode.create(new Object(), "child"+i, 0);
-				parentNode.appendChild(childNode);
-				prevNode.push(parentNode);
-				parentNode = childNode;
-				
-			} else if(treeString.charAt(i) == ')') {
-				
-				prevNode.pop();
-				
-				try{
-		
-					parentNode	=	prevNode.lastElement();	
-				
-				}catch (Exception e) {
-					
-					//TODO
-					
-				}
-				
+			if(i == 0) {
+
+				rootObject 		=	PVDomNode.create(new Object(), "root",  0);
+				parentNode		=	rootObject; 
+
 			} else {
 
-				//TODO 
+				if(treeString.charAt(i) == '(' ) {
+
+					PVDomNode childNode  	= 	PVDomNode.create(new Object(), "child"+i, 0);
+					parentNode.appendChild(childNode);
+					prevNode.push(parentNode);
+					parentNode = childNode;
+
+				} else if(treeString.charAt(i) == ')') {
+
+					try {
+						
+						parentNode	=	prevNode.lastElement();	
+						prevNode.pop();
+					
+					} catch (Exception e) {
+						
+						//TODO	Should rewrite this loop 
+						
+					}
+				}
 			}
-			
 		}
 		return rootObject;
 	}
--- src/org/geworkbenchweb/visualizations/client/ui/VDendrogram.java
+++ src/org/geworkbenchweb/visualizations/client/ui/VDendrogram.java
@@ -1,20 +1,11 @@
 package org.geworkbenchweb.visualizations.client.ui;
 
 import org.thechiselgroup.choosel.protovis.client.PV;
+import org.thechiselgroup.choosel.protovis.client.PVBar;
 import org.thechiselgroup.choosel.protovis.client.PVClusterLayout;
-import org.thechiselgroup.choosel.protovis.client.PVColor;
 import org.thechiselgroup.choosel.protovis.client.PVDomNode;
-import org.thechiselgroup.choosel.protovis.client.PVEventHandler;
-import org.thechiselgroup.choosel.protovis.client.PVLink;
 import org.thechiselgroup.choosel.protovis.client.PVPanel;
 import org.thechiselgroup.choosel.protovis.client.ProtovisWidget;
-import org.vaadin.gwtgraphics.client.DrawingArea;
-import org.vaadin.gwtgraphics.client.Group;
-import org.vaadin.gwtgraphics.client.shape.Rectangle;
-import org.vaadin.gwtgraphics.client.shape.Text;
-import org.thechiselgroup.choosel.protovis.client.jsutil.JsArgs;
-import org.thechiselgroup.choosel.protovis.client.jsutil.JsFunction;
-
 import com.vaadin.terminal.gwt.client.ApplicationConnection;
 import com.vaadin.terminal.gwt.client.Paintable;
 import com.vaadin.terminal.gwt.client.UIDL;
@@ -22,7 +13,6 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.MouseMoveEvent;
 import com.google.gwt.event.dom.client.MouseMoveHandler;
-import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
 
@@ -57,12 +47,6 @@ public class VDendrogram extends Composite implements Paintable, ClickHandler,
 
 	/** Reference to the server connection object. */
 	protected ApplicationConnection client;
-
-	private DrawingArea canvas;
-	
-	private int width = 0;
-	
-	private int height = 0;
 	
 	private AbsolutePanel panel;
 	/**
@@ -73,7 +57,6 @@ public class VDendrogram extends Composite implements Paintable, ClickHandler,
 		
 		panel = new AbsolutePanel();
 		initWidget(panel);
-		canvas = new DrawingArea(width, height);
 		
 		setStyleName(CLASSNAME);
        
@@ -97,169 +80,71 @@ public class VDendrogram extends Composite implements Paintable, ClickHandler,
 		final int arrayNumber 			= 	uidl.getIntVariable("arrayNumber");
 		final int markerNumber 			=	uidl.getIntVariable("markerNumber");
 		
-		width = ((arrayNumber*geneWidth) + 600);
-		height = ((markerNumber*geneHeight) + 600);
+		final int width 	= 	((arrayNumber*geneWidth) + 600);
+		final int height 	= 	((markerNumber*geneHeight) + 600);
+		
+		final String[] colorArray 		=	uidl.getStringArrayVariable("color");
 		
 		/**
 		 * This is where we draw Marker Cluster Dendrogram.
 		 * only if exists.
 		 */
-		if(markerTreeString.contains("(")) {
-			panel.add(new ProtovisWidget() {
-				protected void onAttach() {
-					super.onAttach();
+		
+		panel.add(new ProtovisWidget() {
+			protected void onAttach() {
+				super.onAttach();
 
-					initPVPanel();
+				initPVPanel();
 
-					final String selectedNodeIndexProperty = "selectedNodeIndex";
-			        final String selectedArcIndexProperty = "selectedArcIndex";
-					final PVPanel vis = getPVPanel().width(300).height(markerNumber*geneHeight).left(0).right(0).top(0).bottom(0)
-							 .def(selectedNodeIndexProperty, -1)
-				             .def(selectedArcIndexProperty, null);
-					
-			        
+				//marker dendrogram
+				final PVPanel vis = getPVPanel().width(width).height(height).left(0).right(0).top(0).bottom(0);
+				
+				if(markerTreeString.contains("(")) {
 					PVClusterLayout layout = vis
 							.add(PV.Layout.Cluster())
-							.nodes(((PVDomNode) TreeData.data(markerTreeString)).nodes()).group(false).orient("left");
-
-					final PVColor arcColor = PV.color("rgba(0,0,0,.3)");
-			        final PVColor emphasizedArcColor = PV.color("orange");
-			        final PVColor deemphasizedArcColor = PV.color("rgba(0,0,0,.3)");
-					
-					layout.link().add(PV.Line).lineWidth(1).strokeStyle(new JsFunction<PVColor>() {
-			            public PVColor f(JsArgs args) {
-			                PVLink d = args.getObject(1); // 0 is PVNode
-			               
-			                PVLink selectedArc = vis.getObject(selectedArcIndexProperty);
-			                if (selectedArc != null) {
-			                    return (d == selectedArc) ? emphasizedArcColor
-			                            : deemphasizedArcColor;
-			                }
-
-			                int selectedNodeIndex = vis.getInt(selectedNodeIndexProperty);
-			                if (selectedNodeIndex == -1) {
-			                    return arcColor;
-			                }
-
-			                if (d.source() == selectedNodeIndex
-			                        || d.target() == selectedNodeIndex) {
-			                    return emphasizedArcColor;
-			                }
-			                return deemphasizedArcColor;
-			            }
-			        }).event(PV.Event.MOUSEOVER, new PVEventHandler() {
-
-						@Override
-						public void onEvent(Event e, String pvEventType,
-								JsArgs args) {
-							
-							PVLink d = args.getObject(1);
-			                vis.set(selectedArcIndexProperty, d);
-			                vis.render();
-			                
-						}
-					}).event(PV.Event.MOUSEOUT, new PVEventHandler() {
-			            public void onEvent(Event e, String pvEventType, JsArgs args) {
-			                vis.set(selectedArcIndexProperty, null);
-			                vis.render();
-			            }
-			        }).antialias(false);
-
-					getPVPanel().render();
+							.nodes(((PVDomNode) TreeData.data(markerTreeString)).nodes()).group(false).orient("left")
+							.left(100).top(300).height(markerNumber*geneHeight).width(300);
+					layout.link().add(PV.Line).lineWidth(1)
+					.antialias(false);
 				}
-			}, 100, 300);
-		}
-		
-		/**
-		 * This is where we draw Microarray Cluster Dendrogram.
-		 * Only if exists.
-		 */
-		if(arrayTreeString.contains("(")) {
-			panel.add(new ProtovisWidget() {
-				protected void onAttach() {
-					super.onAttach();
-
-					initPVPanel();
-					
-					PVPanel arrayTree = getPVPanel().width(arrayNumber*geneWidth).height(200).left(0).right(0).top(0).bottom(0);
-					PVClusterLayout arrayTreeLayout = arrayTree
+				
+				if(arrayTreeString.contains("(")) {
+					PVClusterLayout arrayTreeLayout = vis
 							.add(PV.Layout.Cluster())
-							.nodes(((PVDomNode) TreeData.data(arrayTreeString)).nodes()).group(false).orient("top");
-
+							.nodes(((PVDomNode) TreeData.data(arrayTreeString)).nodes()).group(false).orient("top")
+							.left(400).top(100).width(arrayNumber*geneWidth).height(200);
 					arrayTreeLayout.link().add(PV.Line).lineWidth(1)
 					.antialias(false); 
-
-					getPVPanel().render();
 				}
-			}, 400, 100);
-
-		}
-		
-		/*
-		 * Building Heat map starts here.
-		 */
-		canvas.clear();
-		panel.add(canvas, 400, 300);
-        canvas.setWidth(width);
-        canvas.setHeight(height);
-        canvas.getElement().getStyle().setPropertyPx("width", width);
-        canvas.getElement().getStyle().setPropertyPx("height", height);
-
-        int ycord 	= 0;
-        int n = 0;
+				int topCordinate 	=  	300;
+				int leftCordinate	= 	400; 
+				for(int i=0; i<12100; i++) {
+					if(i%arrayNumber == 0) {
+						if(i != 0) {
+							topCordinate 	= 	topCordinate + geneHeight;
+							leftCordinate 	=	400;
+						}	
+					} else {
+						
+						leftCordinate = leftCordinate + geneWidth;
+						
+					}
+					PVBar bar = vis.add(PV.Bar)
+							.top(topCordinate)
+							.left(leftCordinate)
+							.height(geneHeight)
+							.width(geneWidth)
+							.fillStyle("#" + colorArray[i]);
+					
+					/* capture pan & zoom events on main panel */
+			        getPVPanel().event(PV.Event.MOUSEWHEEL, PV.Behavior.zoom());
+				
+				}
+		        
+		        getPVPanel().render();
+			}
+		}, 0, 0);
         
-        Group geneGroup = new Group();
-        for(int i=0; i<(uidl.getStringArrayVariable("color")).length; i++) {
-        	
-        	if(i%arrayNumber == 0) {
-        		
-        		if(i != 0) {
-        			
-        			ycord = ycord + geneHeight;
-        		
-        		}	
-        	
-        	}
-        	/*
-        	 * This is where we start building rectangles for each gene and assign corresponding color.
-        	 */
-        	final Rectangle geneBox = new Rectangle((i%arrayNumber)*geneWidth, ycord, geneWidth, geneHeight);
-    		geneBox.setFillColor((uidl.getStringArrayVariable("color"))[i]);
-    		geneBox.setStrokeColor(null);
-    		geneBox.setStrokeWidth(0);
-    		geneGroup.add(geneBox);
-    	
-    		/*
-			 * Here we add Marker Names
-			 */
-    		
-    		if((i+1)%arrayNumber == 0) {
-    			
-    			Text markerName = new Text(((i%arrayNumber) +1)*geneWidth + 50, ycord+5, uidl.getStringArrayVariable("markerLabels")[n]);
-    			markerName.setFontSize(5);
-    			geneGroup.add(markerName);
-    			n++;
-    			
-    		}
-    		
-    		/*
-    		 * Here we add Array Names
-    		 */
-    		if ((i+1) ==(uidl.getStringArrayVariable("color")).length) {
-    			int xcord = 0;
-    			
-    			for(int j=0; j<(uidl.getStringArrayVariable("arrayLabels")).length; j++) {
-    				if(j != 0){
-    					xcord = xcord + 20;
-    				}
-    				Text arrayName = new Text(xcord, (geneHeight*markerNumber)+25,uidl.getStringArrayVariable("arrayLabels")[j]);
-    				arrayName.setFontSize(6);
-    				arrayName.setRotation(270);
-    				geneGroup.add(arrayName);
-    			}
-    		}
-        }
-        canvas.add(geneGroup);
 	}
 
 	@Override
