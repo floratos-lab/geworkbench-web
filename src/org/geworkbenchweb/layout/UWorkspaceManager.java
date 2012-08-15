f543d0cc7cb3a89c317662cce3b1d457bde263c7
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-08-15 21:39:52 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@9821 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/layout/UWorkspaceManager.java
+++ src/org/geworkbenchweb/layout/UWorkspaceManager.java
@@ -3,14 +3,18 @@ package org.geworkbenchweb.layout;
 import org.geworkbenchweb.dataset.UDataSetUpload;
 import org.vaadin.peter.multibutton.MultiButton;
 
+import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.HorizontalSplitPanel;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.themes.Reindeer;
+import com.vaadin.data.Property.ValueChangeEvent;
 
 /**
  * UWorkspaceManager handles all the workspace details.
@@ -67,6 +71,7 @@ public class UWorkspaceManager extends MultiButton {
 	
 		private static final long serialVersionUID = 867444216969708459L;
 
+		@SuppressWarnings("deprecation")
 		private WorkspaceWindow(){
 			
 			center();
@@ -80,28 +85,76 @@ public class UWorkspaceManager extends MultiButton {
 			setScrollable(false);
 			setResizable(false);
 			
-			HorizontalSplitPanel workPanel = new HorizontalSplitPanel();
+			final HorizontalSplitPanel workPanel = new HorizontalSplitPanel();
 	
 			workPanel.setSizeFull();
 			workPanel.setStyleName(Reindeer.SPLITPANEL_SMALL);
 			workPanel.setSplitPosition(25);
 			workPanel.setMargin(false);
+			workPanel.setLocked(true);
+			workPanel.setImmediate(true);
 			
 			ComboBox workspaces 			= 	new ComboBox();
 			VerticalLayout workSpaceLayout 	=	new VerticalLayout();
 			
+			Panel wActions 		= 	new Panel();
+			Button createNew	=	new Button("Create Workspace");
+			Button deleteWSpace = 	new Button("Delete Workspace");
+			Button importWSpace	=	new Button("Import WorkSpace");
+			
+			createNew.setStyleName(Reindeer.BUTTON_LINK);
+			deleteWSpace.setStyleName(Reindeer.BUTTON_LINK);
+			importWSpace.setStyleName(Reindeer.BUTTON_LINK);
+			
+			wActions.setCaption("Workspace Actions");
+			((AbstractOrderedLayout) wActions.getLayout()).setSpacing(true);
+			wActions.addComponent(createNew);
+			wActions.addComponent(deleteWSpace);
+			wActions.addComponent(importWSpace);
+			
 			workspaces.setInputPrompt("Select Workspace");
 			workspaces.setMultiSelect(false);
 			workspaces.setNullSelectionAllowed(false);
-			workspaces.addItem("Demo Workspace");	
+			workspaces.addItem("Demo Workspace");
+			workspaces.setImmediate(true);
+			workspaces.addListener(new ComboBox.ValueChangeListener() {
+
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public void valueChange(ValueChangeEvent event) {
+					
+					System.out.println("Nikhil");
+					workPanel.setSecondComponent(new ProjectLayout());
+					
+				}
+				
+			});
 			
-			workSpaceLayout.setSizeFull();
 			workSpaceLayout.setMargin(true);
+			workSpaceLayout.setSpacing(true);
 			workSpaceLayout.addComponent(workspaces);
 			workSpaceLayout.setComponentAlignment(workspaces, Alignment.TOP_LEFT);
-			workPanel.setFirstComponent(workSpaceLayout);
+			workSpaceLayout.addComponent(new Label("<br/>", Label.CONTENT_XHTML));
+			workSpaceLayout.addComponent(wActions);
+			workSpaceLayout.setComponentAlignment(wActions, Alignment.MIDDLE_CENTER);
 			
+			workPanel.setFirstComponent(workSpaceLayout);
 			setContent(workPanel);
+		}
+	}
+	
+	private class ProjectLayout extends VerticalLayout {
+
+		private static final long serialVersionUID = -1202432681409804573L;
+
+		public ProjectLayout() {
+			
+			setSizeFull();
+			setMargin(true);
+			setSpacing(true);
+			addComponent(new Label("Projects in the WorkSpace Selected"));
+			
 		}
 	}
 	
