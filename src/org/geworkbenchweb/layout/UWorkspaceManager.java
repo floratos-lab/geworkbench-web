d2daf1e7ddda45d43ef32b6e0182887eebcc2b21
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-08-15 18:28:23 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@9817 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/layout/UWorkspaceManager.java
+++ src/org/geworkbenchweb/layout/UWorkspaceManager.java
@@ -3,9 +3,14 @@ package org.geworkbenchweb.layout;
 import org.geworkbenchweb.dataset.UDataSetUpload;
 import org.vaadin.peter.multibutton.MultiButton;
 
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.HorizontalSplitPanel;
+import com.vaadin.ui.ListSelect;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
+import com.vaadin.ui.themes.Reindeer;
 
 /**
  * UWorkspaceManager handles all the workspace details.
@@ -66,12 +71,37 @@ public class UWorkspaceManager extends MultiButton {
 			
 			center();
 			setModal(true);
-			setHeight("400px");
-			setWidth("600px");
+			setHeight("600px");
+			setWidth("800px");
 			setCaption("Workspace Manager");
 			setClosable(true);
-			setDraggable(true);
+			setDraggable(false);
+			setImmediate(true);
+			setScrollable(false);
+			setResizable(false);
 			
+			HorizontalSplitPanel workPanel = new HorizontalSplitPanel();
+	
+			workPanel.setSizeFull();
+			workPanel.setStyleName(Reindeer.SPLITPANEL_SMALL);
+			workPanel.setSplitPosition(25);
+			workPanel.setMargin(false);
+			
+			ListSelect workspaces 			= 	new ListSelect();
+			VerticalLayout workSpaceLayout 	=	new VerticalLayout();
+			
+			workspaces.setCaption("Select Workspace");
+			workspaces.setMultiSelect(false);
+			workspaces.setNullSelectionAllowed(false);
+			workspaces.addItem("Demo Workspace");	
+			
+			workSpaceLayout.setSizeFull();
+			workSpaceLayout.setMargin(true);
+			workSpaceLayout.addComponent(workspaces);
+			workSpaceLayout.setComponentAlignment(workspaces, Alignment.TOP_LEFT);
+			workPanel.setFirstComponent(workSpaceLayout);
+			
+			setContent(workPanel);
 		}
 	}
 	
