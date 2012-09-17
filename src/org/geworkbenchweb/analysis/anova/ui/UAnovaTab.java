c65e31fecc1a45739194964e736f0bf4eadfdc37
Min You <my2248@c2b2.columbia.edu>
2012-09-17 20:45:57 +0000

    add column formatting
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@9935 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/analysis/anova/ui/UAnovaTab.java
+++ src/org/geworkbenchweb/analysis/anova/ui/UAnovaTab.java
@@ -1,86 +1,102 @@
 package org.geworkbenchweb.analysis.anova.ui;
 
 import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
-import org.geworkbench.bison.datastructure.bioobjects.microarray.CSAnovaResultSet; 
+import org.geworkbench.bison.datastructure.bioobjects.microarray.CSAnovaResultSet;
 
 import org.geworkbenchweb.utils.TableView;
 
- 
 import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.CheckBox; 
-import com.vaadin.ui.GridLayout; 
+import com.vaadin.ui.CheckBox;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.HorizontalLayout; 
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
- 
+
+import com.vaadin.data.Property;
 
 /**
- * This class displays Anova results in a Table 
+ * This class displays Anova results in a Table
+ * 
  * @author Min You
  */
- 
-public class UAnovaTab extends VerticalLayout {   
-	 
+
+public class UAnovaTab extends VerticalLayout {
+
 	private static final long serialVersionUID = 3115606230292029231L;
-	
+
 	private TableView dataTable;
 	// preferences
 	private static boolean fStat = true;
 	private static boolean pVal = true;
 	private static boolean mean = true;
 	private static boolean std = true;
-	
-	private CheckBox bF; 
-	private CheckBox bP; 
-	private CheckBox bM; 
-	private CheckBox bS;  
-	
+
+	private CheckBox bF;
+	private CheckBox bP;
+	private CheckBox bM;
+	private CheckBox bS;
+
 	private CSAnovaResultSet<DSGeneMarker> anovaResultSet = null;
-		 
+
 	Button submitButton;
-	
-	
-	public UAnovaTab(CSAnovaResultSet<DSGeneMarker>  anovaResultSet) {
-	
-	     this.anovaResultSet = anovaResultSet;
-	     
-		 setSpacing(true);
-		 setImmediate(true);	 
-		 
-		 HorizontalLayout horizontalLayout = new HorizontalLayout();
-		 horizontalLayout.setSpacing(true);			 
-	 
-		 horizontalLayout.setImmediate(true);
+
+	public UAnovaTab(CSAnovaResultSet<DSGeneMarker> anovaResultSet) {
+
+		this.anovaResultSet = anovaResultSet;
+
+		setSpacing(true);
+		setImmediate(true);
+
+		HorizontalLayout horizontalLayout = new HorizontalLayout();
+		horizontalLayout.setSpacing(true);
+
+		horizontalLayout.setImmediate(true);
 		/* Results Table Code */
-		 
-		dataTable  = new TableView();		 ;		 
-		dataTable.setContainerDataSource(getIndexedContainer());	
-		
-	 
-		
-		Button preferenceButton;		
-		preferenceButton = new Button("Display Preference", new PreferenceListener());
-		
-		Button exportButton;		
+
+		dataTable = new TableView() {
+
+			private static final long serialVersionUID = 9129226094428040540L;
+
+			@Override
+			protected String formatPropertyValue(Object rowId, Object colId,
+					Property property) {
+				Object value = property.getValue();
+				if ((value != null) && (value instanceof Number)) {
+					if (((Number) value).doubleValue() < 0.1)
+						return String.format("%.2E", value);
+					else
+						return String.format("%.2f", value);
+				}
+
+				return super.formatPropertyValue(rowId, colId, property);
+			}
+		};
+
+		dataTable.setContainerDataSource(getIndexedContainer());
+
+		Button preferenceButton;
+		preferenceButton = new Button("Display Preference",
+				new PreferenceListener());
+
+		Button exportButton;
 		exportButton = new Button("Export", new ExportListener());
-	 
+
 		horizontalLayout.addComponent(preferenceButton, 0);
 		horizontalLayout.addComponent(exportButton, 1);
-		
-			 
+
 		addComponent(horizontalLayout);
-		addComponent(dataTable);	
-	 
+		addComponent(dataTable);
+
 	}
-	
+
 	private IndexedContainer getIndexedContainer() {
 
 		String[] header;
 		IndexedContainer dataIn = new IndexedContainer();
-		
+
 		int groupNum = anovaResultSet.getLabels(0).length;
 		int meanStdStartAtIndex = 1 + (fStat ? 1 : 0) + (pVal ? 1 : 0);
 		header = new String[meanStdStartAtIndex + groupNum
@@ -105,153 +121,142 @@ public class UAnovaTab extends VerticalLayout {
 						.getLabels(0)[cx] + "_Std";
 			}
 		}
-		
-		for (String p : header) 
-		{ 
+
+		for (String p : header) {
 			if (p.equals("Marker Name"))
-		       dataIn.addContainerProperty(p, String.class, "");   
+				dataIn.addContainerProperty(p, String.class, "");
 			else
-				dataIn.addContainerProperty(p, Float.class, "");  
-			  
+				dataIn.addContainerProperty(p, Float.class, "");
+
 		}
-		
-		
+
 		double[][] result2DArray = anovaResultSet.getResult2DArray();
 		int significantMarkerNumbers = anovaResultSet.getSignificantMarkers()
 				.size();
 		for (int cx = 0; cx < significantMarkerNumbers; cx++) {
 			Object id = dataIn.addItem();
 			fieldIndex = 0;
-			dataIn.getContainerProperty(id, header[fieldIndex++]).setValue(((DSGeneMarker) anovaResultSet
-					.getSignificantMarkers().get(cx)).getShortName());
+			dataIn.getContainerProperty(id, header[fieldIndex++]).setValue(
+					((DSGeneMarker) anovaResultSet.getSignificantMarkers().get(
+							cx)).getShortName());
 			if (pVal) {
-				dataIn.getContainerProperty(id, header[fieldIndex++]).setValue(convertDouble(result2DArray[0][cx]));
+				dataIn.getContainerProperty(id, header[fieldIndex++]).setValue(
+						result2DArray[0][cx]);
 			}
 			if (fStat) {
-				dataIn.getContainerProperty(id, header[fieldIndex++]).setValue(convertDouble(result2DArray[2][cx]));
+				dataIn.getContainerProperty(id, header[fieldIndex++]).setValue(
+						result2DArray[2][cx]);
 			}
 			for (int gc = 0; gc < groupNum; gc++) {
 				if (mean) {
-					dataIn.getContainerProperty(id, header[meanStdStartAtIndex + gc
-					           							* ((mean ? 1 : 0) + (std ? 1 : 0)) + 0]).setValue(convertDouble(result2DArray[3 + gc * 2][cx]));
+					dataIn.getContainerProperty(
+							id,
+							header[meanStdStartAtIndex + gc
+									* ((mean ? 1 : 0) + (std ? 1 : 0)) + 0])
+							.setValue(result2DArray[3 + gc * 2][cx]);
 				}
 				if (std) {
-					dataIn.getContainerProperty(id, header[meanStdStartAtIndex + gc
-							* ((mean ? 1 : 0) + (std ? 1 : 0)) + (mean ? 1 : 0)]).setValue(convertDouble(result2DArray[4 + gc * 2][cx]));
+					dataIn.getContainerProperty(
+							id,
+							header[meanStdStartAtIndex + gc
+									* ((mean ? 1 : 0) + (std ? 1 : 0))
+									+ (mean ? 1 : 0)]).setValue(
+							result2DArray[4 + gc * 2][cx]);
 				}
 			}
-			 
+
 		}
-		
-		
+
 		return dataIn;
 	}
-	
+
 	 
-	
-	private Object convertDouble(Object value) {
-		     
-		if ((value != null) && (value instanceof Number)) {
-			if (((Number) value).doubleValue() < 0.1)
-				value = String.format("%.2E", value);
-			else
-				value = String.format("%.2f", value);
-		}
-			
-			return value;
-			
-	}
-		 
-	
-	 
+
 	private class PreferenceListener implements ClickListener {
 
 		private static final long serialVersionUID = 831124091338570481L;
-	 
-		public PreferenceListener (){};
-		 
+
+		public PreferenceListener() {
+		};
+
 		@Override
 		public void buttonClick(ClickEvent event) {
-			 
-			Window dispPref  = new Window("Display Preferences");
-		 
-			GridLayout  gridLayout	= 	new GridLayout(2, 2);
-			
+
+			Window dispPref = new Window("Display Preferences");
+
+			GridLayout gridLayout = new GridLayout(2, 2);
+
 			dispPref.setModal(true);
 			dispPref.setClosable(true);
 			dispPref.setDraggable(false);
 			dispPref.setResizable(false);
 			dispPref.setWidth("350px");
-			
+
 			bF = new CheckBox("F-Statistic", fStat);
-		    bP = new CheckBox("P-Value", pVal);
+			bP = new CheckBox("P-Value", pVal);
 			bM = new CheckBox("Mean", mean);
-		    bS = new CheckBox("Std", std);
-		    
-		    bF.addListener(new CheckBoxListener());
-		    bP.addListener(new CheckBoxListener());
-		    bM.addListener(new CheckBoxListener());
-		    bS.addListener(new CheckBoxListener());
-			
-		    gridLayout.setMargin(true);
-		    gridLayout.setImmediate(true);
-		    gridLayout.setSpacing(true);
-			
-		    gridLayout.addComponent(bF, 0, 0);
-		    gridLayout.addComponent(bP, 1, 0);
-		    gridLayout.addComponent(bM, 0, 1);
-		    gridLayout.addComponent(bS, 1, 1);
-			 
+			bS = new CheckBox("Std", std);
+
+			bF.addListener(new CheckBoxListener());
+			bP.addListener(new CheckBoxListener());
+			bM.addListener(new CheckBoxListener());
+			bS.addListener(new CheckBoxListener());
+
+			gridLayout.setMargin(true);
+			gridLayout.setImmediate(true);
+			gridLayout.setSpacing(true);
+
+			gridLayout.addComponent(bF, 0, 0);
+			gridLayout.addComponent(bP, 1, 0);
+			gridLayout.addComponent(bM, 0, 1);
+			gridLayout.addComponent(bS, 1, 1);
+
 			dispPref.addComponent(gridLayout);
 			getApplication().getMainWindow().addWindow(dispPref);
 		}
-		 
+
 	}
-	
+
 	private class ExportListener implements ClickListener {
 
 		private static final long serialVersionUID = 831124091338570481L;
-	 
-	 
-		public ExportListener ( ){};
-	 
+
+		public ExportListener() {
+		};
+
 		@Override
 		public void buttonClick(ClickEvent event) {
-			
-			 dataTable.csvExport("anovaTable.csv");			 
-			
-         } 
+
+			dataTable.csvExport("anovaTable.csv");
+
+		}
 	}
-	
-	
+
 	private class CheckBoxListener implements ClickListener {
 
 		private static final long serialVersionUID = 831124091338570481L;
-	 
-	 
-		public CheckBoxListener(){};
-	 
+
+		public CheckBoxListener() {
+		};
+
 		@Override
 		public void buttonClick(ClickEvent e) {
-			  
+
 			Object source = e.getSource();
- 
-	        if (source == bF) {
-	        	fStat = bF.booleanValue();
-	        } else if (source == bP) {
-	        	pVal = bP.booleanValue();
-	        } else if (source == bM) {
-	        	mean = bM.booleanValue();
-	        } else if (source == bS) {
-	        	std = bS.booleanValue();
-	        }
-	        
-	        dataTable.setContainerDataSource(getIndexedContainer());	
-			
-			
-          } 
+
+			if (source == bF) {
+				fStat = bF.booleanValue();
+			} else if (source == bP) {
+				pVal = bP.booleanValue();
+			} else if (source == bM) {
+				mean = bM.booleanValue();
+			} else if (source == bS) {
+				std = bS.booleanValue();
+			}
+
+			dataTable.setContainerDataSource(getIndexedContainer());
+
+		}
 	}
-	
-	 
-	 
+
 }
