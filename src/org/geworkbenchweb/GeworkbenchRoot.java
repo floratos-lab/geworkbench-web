670d8a2ed7293df2c223352dd650fb30199ca01d
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-09-28 15:45:02 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@9948 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/GeworkbenchRoot.java
+++ src/org/geworkbenchweb/GeworkbenchRoot.java
@@ -1,11 +1,18 @@
 package org.geworkbenchweb;
 
+import java.net.URI;
+
 import org.geworkbenchweb.authentication.UUserAuth;
+import org.geworkbenchweb.events.AnalysisSubmissionEvent;
+import org.geworkbenchweb.events.AnalysisSubmissionEvent.AnalysisSubmissionEventListener;
 import org.geworkbenchweb.events.NodeAddEvent;
 import org.geworkbenchweb.events.NodeAddEvent.NodeAddEventListener;
+import org.geworkbenchweb.events.PluginEvent;
+import org.geworkbenchweb.events.PluginEvent.PluginEventListener;
 import org.geworkbenchweb.layout.UMainLayout;
 import org.vaadin.appfoundation.authentication.SessionHandler;
 import org.vaadin.appfoundation.authentication.data.User;
+import org.vaadin.artur.icepush.ICEPush;
 
 import com.github.wolfie.blackboard.Blackboard;
 import com.vaadin.Application;
@@ -26,14 +33,24 @@ public class GeworkbenchRoot extends Application implements TransactionListener 
 	
 	private final Blackboard blackboardInstance = new Blackboard();
 	
+	private static ThreadLocal<ICEPush> PUSHER = new ThreadLocal<ICEPush>();
+	
+	private final ICEPush pusherInstance = new ICEPush();
+	
 	private Window mainWindow;
+	
+	private static final String SAMPLER_THEME_NAME = "geworkbench";
+
+    // used when trying to guess theme location
+    private static String APP_URL = null;
 
 	@Override
 	public void init() {
 		
 		BLACKBOARD.set(blackboardInstance);
+		PUSHER.set(pusherInstance);
 		
-		setTheme("geworkbench");
+		setTheme(SAMPLER_THEME_NAME);
 		SessionHandler.initialize(this);
 		
 		User user 	= 	SessionHandler.get();
@@ -41,7 +58,6 @@ public class GeworkbenchRoot extends Application implements TransactionListener 
 		
 		mainWindow.setSizeFull();
 		setMainWindow(mainWindow);
-		
 		getContext().addTransactionListener(this);
 		registerAllEventsForApplication();
 		
@@ -50,8 +66,23 @@ public class GeworkbenchRoot extends Application implements TransactionListener 
 		}else {	
 			loginView();
 		}
-		
 	}
+	
+	/**
+     * Tries to guess theme location.
+     * 
+     * @return
+     */
+    public static String getThemeBase() {
+        try {
+            URI uri = new URI(APP_URL + "../VAADIN/themes/"
+                    + SAMPLER_THEME_NAME + "/");
+            return uri.normalize().toString();
+        } catch (Exception e) {
+            System.err.println("Theme location could not be resolved:" + e);
+        }
+        return "/VAADIN/themes/" + SAMPLER_THEME_NAME + "/";
+    }
 		
 	public void loginView() {
 		mainWindow.setContent(new UUserAuth());
@@ -64,17 +95,19 @@ public class GeworkbenchRoot extends Application implements TransactionListener 
 	@Override
 	public void transactionStart(Application application, Object transactionData) {
 		if (application == this) {
-		      BLACKBOARD.set(blackboardInstance);
-		    }
+			BLACKBOARD.set(blackboardInstance);
+			PUSHER.set(pusherInstance);
+		}
 	}
 
 	@Override
 	public void transactionEnd(Application application, Object transactionData) {
-		 if (application == this) {
-		      // to avoid keeping an Application hanging, and mitigate the 
-		      // possibility of user session crosstalk
-		      BLACKBOARD.set(null);
-		    }
+		if (application == this) {
+			// to avoid keeping an Application hanging, and mitigate the 
+			// possibility of user session crosstalk
+			BLACKBOARD.set(null);
+			PUSHER.set(null);
+		}
 	}
 	
 	/**
@@ -85,6 +118,10 @@ public class GeworkbenchRoot extends Application implements TransactionListener 
 		return BLACKBOARD.get();
 	}
 	
+	public static ICEPush getPusher() {
+		return PUSHER.get();
+	}
+	
 	/**
 	 * All the Events in geWorkbench Application are strictly registered here.
 	 */
@@ -92,6 +129,8 @@ public class GeworkbenchRoot extends Application implements TransactionListener 
 		
 		/* This event should be fired whenever new ResultNode is added */
 		getBlackboard().register(NodeAddEventListener.class, NodeAddEvent.class);
+		getBlackboard().register(PluginEventListener.class, PluginEvent.class);
+		getBlackboard().register(AnalysisSubmissionEventListener.class, AnalysisSubmissionEvent.class);
 	}
 
 }
