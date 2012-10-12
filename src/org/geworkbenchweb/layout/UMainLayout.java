519011cb7592d59e921e0d93458f01a8979d313a
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-10-12 18:47:04 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@10031 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/layout/UMainLayout.java
+++ src/org/geworkbenchweb/layout/UMainLayout.java
@@ -1,25 +1,15 @@
 package org.geworkbenchweb.layout;
 
 import java.io.File;
-import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Vector;
-
 import org.geworkbench.bison.datastructure.biocollections.microarrays.DSMicroarraySet;
-import org.geworkbench.bison.datastructure.biocollections.views.CSMicroarraySetView;
-import org.geworkbench.bison.datastructure.biocollections.views.DSMicroarraySetView;
-import org.geworkbench.bison.datastructure.bioobjects.markers.DSGeneMarker;
 import org.geworkbench.bison.datastructure.bioobjects.markers.annotationparser.Affy3ExpressionAnnotationParser;
 import org.geworkbench.bison.datastructure.bioobjects.markers.annotationparser.AffyAnnotationParser;
 import org.geworkbench.bison.datastructure.bioobjects.markers.annotationparser.AnnotationParser;
-import org.geworkbench.bison.datastructure.bioobjects.microarray.DSMicroarray;
-import org.geworkbench.bison.model.clusters.CSHierClusterDataSet;
-import org.geworkbench.bison.model.clusters.HierCluster;
-import org.geworkbench.util.network.CellularNetWorkElementInformation;
 import org.geworkbenchweb.GeworkbenchRoot; 
 import org.geworkbenchweb.events.AnalysisSubmissionEvent;
 import org.geworkbenchweb.events.AnalysisSubmissionEvent.AnalysisSubmissionEventListener;
@@ -67,9 +57,7 @@ import com.vaadin.ui.themes.Reindeer;
 import de.steinwedel.vaadin.MessageBox;
 import de.steinwedel.vaadin.MessageBox.ButtonType;
 
-import org.geworkbenchweb.plugins.cnkb.CNKBInteractions;
-import org.geworkbenchweb.plugins.hierarchicalclustering.HierarchicalClusteringParams;
-import org.geworkbenchweb.plugins.hierarchicalclustering.HierarchicalClusteringWrapper;
+import org.geworkbenchweb.plugins.Analysis;
 import org.geworkbenchweb.plugins.microarray.Microarray;
 
 /**
@@ -518,12 +506,10 @@ public class UMainLayout extends VerticalLayout {
 					}
 					dataSets.setChildrenAllowed(subSetId, false);
 					dataSets.setParent(subSetId, dataId);
-
 				}
 			}
 		}
 		return dataSets;
-
 	}
 
 	/**
@@ -540,7 +526,6 @@ public class UMainLayout extends VerticalLayout {
 		mainItem.getItemProperty("Labels").setValue("Phenotypes");
 
 		for(int k=0;k<maSet.size();k++) {
-
 			Item item 					= 	tableData.addItem(k);
 			tableData.setChildrenAllowed(k, false);
 			item.getItemProperty("Labels").setValue(maSet.get(k).getLabel());
@@ -681,43 +666,13 @@ public class UMainLayout extends VerticalLayout {
 
 		@Override
 		public void SubmitAnalysis(final AnalysisSubmissionEvent event) {
-
-			final ResultSet resultSet = event.getResultSet();
-			final HashMap<Serializable, Serializable> params = event.getParameters();
+		
 			pusher.setEnabled(true);
 			Thread analysis = new Thread() {
 				public void run() {
 					synchronized(getApplication()) {
-						DSMicroarraySet dataSet = null;
-						try {
-							 dataSet = (DSMicroarraySet) event.getDataSet();
-						} catch (Exception e) {
-							//TODO
-						}
-						if(resultSet.getType().contains("HierarchicalClusteringResults")) {
-							DSMicroarraySetView<DSGeneMarker, DSMicroarray> data = 
-									new CSMicroarraySetView<DSGeneMarker, DSMicroarray>(dataSet);
-							HierarchicalClusteringWrapper analysis 	= 	
-									new HierarchicalClusteringWrapper(data, (Integer) params.get(HierarchicalClusteringParams.CLUSTER_METRIC), 
-											(Integer) params.get(HierarchicalClusteringParams.CLUSTER_METHOD), 
-											(Integer) params.get(HierarchicalClusteringParams.CLUSTER_DIMENSION));
-							HierCluster[] resultClusters = analysis.execute();
-							CSHierClusterDataSet results = new CSHierClusterDataSet(resultClusters, null, false,
-									"Hierarchical Clustering", data);
-							resultSet.setName("Hierarchical Clustering");
-							resultSet.setData(ObjectConversion.convertToByte(results));
-						} else if(resultSet.getType().contains("CNKBResults")) {
-							CNKBInteractions cnkb = new CNKBInteractions();
-							Vector<CellularNetWorkElementInformation> hits = cnkb.CNKB(dataSet, params, dataSetId);
-							resultSet.setName("CNKB");
-							resultSet.setData(ObjectConversion.convertToByte(hits));
-						} else if(resultSet.getType().contains("AnovaResults")) {
-							
-						} else if(resultSet.getType().contains("AracneResults")) {
-							
-						} else {
-							//Marina
-						}
+						Analysis analysis = new Analysis();
+						final ResultSet resultSet = analysis.execute(event);
 						FacadeFactory.getFacade().store(resultSet);	
 						MessageBox mb = new MessageBox(getWindow(), 
 								"Analysis Completed", 
