94751c9c9b5344273ab9e8ed186fe0d1dc505758
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-10-12 16:46:16 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@10024 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/plugins/microarray/MicroarrayUI.java
+++ src/org/geworkbenchweb/plugins/microarray/MicroarrayUI.java
@@ -334,7 +334,10 @@ public class MicroarrayUI extends VerticalLayout {
 		cnkbCssLayout.addStyleName("lay");
 		
 		final Label cnkbText = new Label(
-				"<p align = \"justify\">MARINa Analysis</p>");
+				"<p align = \"justify\">The Cellular Network Knowledge Base (CNKB) is a repository of molecular interactions, " +
+				"including ones both computationally and experimentally derived. Sources for interactions " +
+				"include both publicly available databases such as BioGRID and HPRD, as well as reverse-engineered " +
+				"cellular regulatory interactomes developed in the lab of Dr. Andrea Califano at Columbia University.</p>");
 		cnkbText.setContentMode(Label.CONTENT_XHTML);
 		
 		final Button cnkbButton = new Button();
@@ -454,6 +457,81 @@ public class MicroarrayUI extends VerticalLayout {
 		tableLayout.setSpacing(true);
 		tableLayout.addComponent(table);
 		tableLayout.addComponent(tableButton);
+		
+		/**
+		 * DataSet Annotation
+		 */
+		
+		Label annotation = new Label("DataSet history and Annotation");
+		annotation.setStyleName(Reindeer.LABEL_H2);
+		annotation.setContentMode(Label.CONTENT_PREFORMATTED);
+		addComponent(annotation);
+		
+		final GridLayout annotLayout 		=	new GridLayout();
+		final Button annotButton 			= 	new Button();
+		final Button annotCancelButton 		= 	new Button();	
+		final FancyCssLayout annotCssLayout = 	new FancyCssLayout();
+		
+		annotLayout.setColumns(2);
+		annotLayout.setRows(2);
+		annotLayout.setSizeFull();
+		annotLayout.setImmediate(true);
+		annotLayout.setColumnExpandRatio(1, 1.0f);
+
+		annotCssLayout.setWidth("95%");
+		annotCssLayout.setSlideEnabled(true);
+		annotCssLayout.addStyleName("lay");
+		
+		Button annot 	= 	new Button("DataSet Annotation", new Button.ClickListener() {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				PluginEvent loadPlugin = new PluginEvent("Annotation", dataId);
+				GeworkbenchRoot.getBlackboard().fire(loadPlugin);
+			}
+		});
+		
+		final Label annotText = new Label(
+				"<p align = \"justify\">This is DataSet Annotation plugin</p>");
+		annotText.setContentMode(Label.CONTENT_XHTML);
+		
+		annotButton.addListener(new Button.ClickListener() {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				annotCssLayout.removeAllComponents();
+				annotLayout.removeComponent(annotButton);
+				annotLayout.addComponent(annotCancelButton, 1, 0);
+				annotCssLayout.addComponent(annotText);
+				annotLayout.addComponent(annotCssLayout, 0, 1, 1, 1);
+			}
+		});
+		annotCancelButton.addListener(new Button.ClickListener() {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				annotCssLayout.removeAllComponents();
+				annotLayout.removeComponent(annotCancelButton);
+				annotLayout.addComponent(annotButton, 1, 0);
+				annotLayout.removeComponent(annotCssLayout);
+			}
+		});
+		
+		annot.setStyleName(Reindeer.BUTTON_LINK);
+		annotButton.setStyleName(BaseTheme.BUTTON_LINK);
+		annotButton.setIcon(ICON);
+		annotCancelButton.setStyleName(BaseTheme.BUTTON_LINK);
+		annotCancelButton.setIcon(CancelIcon);
+		addComponent(annotLayout);
+		annotLayout.setSpacing(true);
+		annotLayout.addComponent(annot);
+		annotLayout.addComponent(annotButton);
 		
 		
 		/**
