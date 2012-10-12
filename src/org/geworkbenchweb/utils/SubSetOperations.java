ecdc12c3dce0c4eddf3d42c571fb439d932ac95c
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-10-12 21:56:11 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@10035 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/utils/SubSetOperations.java
+++ src/org/geworkbenchweb/utils/SubSetOperations.java
@@ -16,94 +16,90 @@ import org.vaadin.appfoundation.persistence.facade.FacadeFactory;
 
 public class SubSetOperations {
 
-	
+
 	public static boolean storeData(ArrayList<String> arrayList, String setType,
 			String name, long l) {
-		
+
 		SubSet subset  	= 	new SubSet();
-		
+
 		subset.setName(name);
 		subset.setType(setType);
 		subset.setOwner(SessionHandler.get().getId());
-	    subset.setParent(l);
-	    subset.setPositions(arrayList);
-		
-		try {
+		subset.setParent(l);
+		subset.setPositions(arrayList);
 
-		    FacadeFactory.getFacade().store(subset);
-			
+		try {
+			FacadeFactory.getFacade().store(subset);
 		} catch (Exception e) {
-		
-			return false;
-			
+			return false;	
 		}
-		
+
 		return true;
 	}
 
 	public static List<?> getMarkerSet(Long setId) {
-		
+
 		Map<String, Object> parameters 	= 	new HashMap<String, Object>();
-		
+
 		parameters.put("id", setId);
 		parameters.put("type", "marker");
-		
+
 		List<?> data = FacadeFactory.getFacade().list("Select p from SubSet as p where p.id=:id " +
 				"and p.type=:type", parameters);
 		return data;
 	}
-	
+
 	public static List<?> getArraySet(Long setId) {
-		
+
 		Map<String, Object> parameters 	= 	new HashMap<String, Object>();
-		
+
 		parameters.put("id", setId);
 		parameters.put("type", "microarray");
-		
+
 		List<?> data = FacadeFactory.getFacade().list("Select p from SubSet as p where p.id=:id " +
 				"and p.type=:type", parameters);
 		return data;
 	}
-	
+
 	public static List<?> getMarkerSets(Long dataSetId) {
-		
+
 		Map<String, Object> parameters 	= 	new HashMap<String, Object>();
-		
+
 		parameters.put("parent", dataSetId);
 		parameters.put("type", "marker");
-		
+
 		List<?> data = FacadeFactory.getFacade().list("Select p from SubSet as p where p.parent=:parent and p.type=:type ", parameters);
-		
+
 		return data;
 	}
-	
 
-public static int getSignificanceSetNum(Long dataSetId) {
-		
-	Map<String, Object> parameters 	= 	new HashMap<String, Object>();
-	
-	parameters.put("parent", dataSetId);
-	parameters.put("type", "marker");
-	parameters.put("name", "Significan Genes%");
-	
-	List<?> data = FacadeFactory.getFacade().list("Select p from SubSet as p where p.parent=:parent and p.type=:type and p.name like :name", parameters);
-	
-	if (data != null)	  
-	   return data.size();
-	else
-		return 0;
-}
-	
-	
-     public static List<?> getArraySets(Long dataSetId) {
-		
+
+	public static int getSignificanceSetNum(Long dataSetId) {
+
 		Map<String, Object> parameters 	= 	new HashMap<String, Object>();
-		
+
 		parameters.put("parent", dataSetId);
-		parameters.put("type", "Microarray");
-		
+		parameters.put("type", "marker");
+		parameters.put("name", "Significan Genes%");
+
+		List<?> data = FacadeFactory.getFacade().list("Select p from SubSet as p where p.parent=:parent and p.type=:type and p.name like :name", parameters);
+
+		if (data != null)	  
+			return data.size();
+		else
+			return 0;
+	}
+
+
+	public static List<?> getArraySets(Long dataSetId) {
+
+		Map<String, Object> parameters 	= 	new HashMap<String, Object>();
+
+		parameters.put("parent", dataSetId);
+		parameters.put("type", "microarray");
+
 		List<?> data = FacadeFactory.getFacade().list("Select p from SubSet as p where p.parent=:parent and p.type=:type ", parameters);
-		
+
 		return data;
 	}
 
@@ -113,24 +109,24 @@ public static int getSignificanceSetNum(Long dataSetId) {
 	 * 
 	 */
 	public static void deleteAllSets(Long dataSetId) {
-		
-		
-		
+
+
+
 	}
-	
+
 	public static boolean checkForDataSet(String subSetName) {
-		
+
 		Map<String, Object> parameters 	= 	new HashMap<String, Object>();
-		
+
 		parameters.put("name", subSetName);
-		
+
 		List<?> data =  FacadeFactory.getFacade().list("Select p from SubSet as p where p.name=:name", parameters);
-		
+
 		if(data.isEmpty()) {
 			return false;
 		}else {
 			return true;
 		}
-	
+
 	}
 }
