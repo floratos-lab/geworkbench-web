962b7d2dfcd0bb5630e8a2f44c1801276018009b
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-10-16 18:10:51 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@10047 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/plugins/uploaddata/UploadDataUI.java
+++ src/org/geworkbenchweb/plugins/uploaddata/UploadDataUI.java
@@ -1,11 +1,13 @@
 package org.geworkbenchweb.plugins.uploaddata;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.geworkbenchweb.dataset.DataSetParser;
+import org.geworkbenchweb.dataset.geo.GEODataFetch;
 import org.geworkbenchweb.pojos.Project;
 import org.geworkbenchweb.utils.WorkspaceUtils;
 import org.vaadin.appfoundation.authentication.SessionHandler;
@@ -15,9 +17,11 @@ import org.vaadin.easyuploads.UploadField;
 import org.vaadin.easyuploads.UploadField.FieldType;
 import org.vaadin.jonatan.contexthelp.ContextHelp;
 
+import com.vaadin.data.Property;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.AbstractSelect.Filtering;
 
@@ -25,7 +29,7 @@ public class UploadDataUI extends VerticalLayout {
 
 	private static final long serialVersionUID = 1L;
 
-	private static final String[] files		= 	new String[] { "Expression File", "PDB File" };
+	private static final String[] files		= 	new String[] { "Expression File", "GEO SOFT", "PDB File" };
     private static final String initialText = 	"Enter description here.";
    
     private ComboBox fileCombo; 			
@@ -33,6 +37,8 @@ public class UploadDataUI extends VerticalLayout {
     private UploadField uploadField; 		
     private UploadField annotUploadField;
     private ComboBox projectBox;
+    private ComboBox uploadType;
+    private TextField geoTextField;
     
 	public UploadDataUI(Long dataSetId) {
 		
@@ -40,11 +46,13 @@ public class UploadDataUI extends VerticalLayout {
 		help.setFollowFocus(true);
 		addComponent(help);
 		
+		uploadType			=	new ComboBox("Please select uplaod type");
 		projectBox			=	new ComboBox("Please select project");
 		fileCombo 			= 	new ComboBox("Please select type of file");
 		dataArea 			= 	new TextArea(null, initialText);
 		uploadField 		= 	new UploadField();
 		annotUploadField	=	new UploadField();
+		geoTextField		=	new TextField("Enter GEO ID");
 
 		help.addHelpForComponent(projectBox, "Project for the data to be uploaded");
 		help.addHelpForComponent(fileCombo, "Select the type of file");
@@ -60,17 +68,41 @@ public class UploadDataUI extends VerticalLayout {
 		projectBox.setNullSelectionAllowed(false);
 
 		setProjects();
+		
+		uploadType.setFilteringMode(Filtering.FILTERINGMODE_OFF);
+		uploadType.setImmediate(true);
+		uploadType.setRequired(true);
+		uploadType.setNullSelectionAllowed(false);
+		uploadType.addItem("Upload from your Desktop");
+		uploadType.addItem("Import from NCBI GEO");
+		uploadType.addListener(new Property.ValueChangeListener() {
+
+			private static final long serialVersionUID = 1L;
+			public void valueChange(Property.ValueChangeEvent valueChangeEvent) {
+				try {
+					String type = valueChangeEvent.getProperty().getValue().toString();
+					if(type == "Upload from your Desktop") {
+						
+					} else {
+						fileCombo.setEnabled(false);
+						uploadField.setEnabled(false);
+					}
+				}catch (Exception e) {
+				}
+			}
+		});
 
 		fileCombo.setFilteringMode(Filtering.FILTERINGMODE_OFF);
 		fileCombo.setImmediate(true);
 		fileCombo.setRequired(true);
 		fileCombo.setNullSelectionAllowed(false);
-
 		dataArea.setRows(6);
 		dataArea.setColumns(40);
 
 		addComponent(projectBox);
+		addComponent(uploadType);
 		addComponent(fileCombo);
+		addComponent(geoTextField);
 		addComponent(dataArea);
 
 		uploadField.setFieldType(FieldType.FILE);
@@ -121,13 +153,24 @@ public class UploadDataUI extends VerticalLayout {
 
 	public void theButtonClick(Button.ClickEvent event) {
     	
-    	String projectName		=  	(String) projectBox.getValue();
-    	String fileType 		= 	(String) fileCombo.getValue();
-        String dataDescription 	= 	(String) dataArea.getValue();
-    	File dataFile 			= 	(File) uploadField.getValue();
-    	
-    	parseInit(dataFile, null, fileType, projectName, dataDescription);
-    	dataFile.delete();
+		String projectName		=  	(String) projectBox.getValue();
+		String dataDescription 	= 	(String) dataArea.getValue();
+		if(geoTextField.getValue().toString() == null) {
+    		
+    		String fileType 		= 	(String) fileCombo.getValue();
+    		File dataFile 			= 	(File) uploadField.getValue();
+
+    		parseInit(dataFile, null, fileType, projectName, dataDescription);
+    		dataFile.delete();
+    	} else {
+    		try {
+				File dataFile = GEODataFetch.getGDS(geoTextField.getValue().toString());
+				parseInit(dataFile, null, "GDS", projectName, dataDescription);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
     }
     
 	protected void parseInit(File dataFile, File annotFile, String fileType,  String projectName, String dataDescription) {
