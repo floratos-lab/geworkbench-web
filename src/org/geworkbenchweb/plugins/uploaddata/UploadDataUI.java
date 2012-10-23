27c5dcd6c578d197c6cf6b9938411833dbecbbc6
Nikhil Reddy Podduturi <np2417@c2b2.columbia.edu>
2012-10-23 21:11:09 +0000

    Disabled GEO download for now....This should be taken care only after refactoring GEO parsers in the geWorkbench Swing version to work with web version.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@10089 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/plugins/uploaddata/UploadDataUI.java
+++ src/org/geworkbenchweb/plugins/uploaddata/UploadDataUI.java
@@ -1,17 +1,13 @@
 package org.geworkbenchweb.plugins.uploaddata;
 
 import java.io.File;
-import java.io.IOException;
 import org.geworkbenchweb.dataset.DataSetParser;
-import org.geworkbenchweb.dataset.geo.GEODataFetch;
 import org.vaadin.easyuploads.FileFactory;
 import org.vaadin.easyuploads.UploadField;
 import org.vaadin.easyuploads.UploadField.FieldType;
-import com.vaadin.data.Property;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.TextArea;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.AbstractSelect.Filtering;
 
@@ -19,30 +15,30 @@ public class UploadDataUI extends VerticalLayout {
 
 	private static final long serialVersionUID = 1L;
 
-	private static final String[] files		= 	new String[] { "Expression File", "GEO SOFT", "PDB File" };
+	private static final String[] files		= 	new String[] { "Expression File", "PDB File" };
     private static final String initialText = 	"Enter description here.";
    
     private ComboBox fileCombo; 			
     private TextArea dataArea;			
     private UploadField uploadField; 		
     private UploadField annotUploadField;
-    private ComboBox uploadType;
-    private TextField geoTextField;
+    //private ComboBox uploadType;
+    //private TextField geoTextField;
     
 	public UploadDataUI(Long dataSetId) {
 		
-		uploadType			=	new ComboBox("Please select upload type");
+		//uploadType			=	new ComboBox("Please select upload type");
 		fileCombo 			= 	new ComboBox("Please select type of file");
 		dataArea 			= 	new TextArea(null, initialText);
 		uploadField 		= 	new UploadField();
 		annotUploadField	=	new UploadField();
-		geoTextField		=	new TextField("Enter GEO ID");
+		//geoTextField		=	new TextField("Enter GEO ID");
 		
 		for (int i = 0; i < files.length; i++) {
 			fileCombo.addItem(files[i]);    
 		}
 
-		uploadType.setFilteringMode(Filtering.FILTERINGMODE_OFF);
+	/*	uploadType.setFilteringMode(Filtering.FILTERINGMODE_OFF);
 		uploadType.setImmediate(true);
 		uploadType.setRequired(true);
 		uploadType.setNullSelectionAllowed(false);
@@ -66,7 +62,7 @@ public class UploadDataUI extends VerticalLayout {
 				}catch (Exception e) {
 				}
 			}
-		});
+		});*/
 
 		fileCombo.setFilteringMode(Filtering.FILTERINGMODE_OFF);
 		fileCombo.setImmediate(true);
@@ -75,10 +71,10 @@ public class UploadDataUI extends VerticalLayout {
 		dataArea.setRows(6);
 		dataArea.setColumns(40);
 
-		addComponent(uploadType);
+		//addComponent(uploadType);
 		addComponent(fileCombo);
-		addComponent(geoTextField);
-		addComponent(dataArea);
+		//addComponent(geoTextField);
+		//addComponent(dataArea);
 
 		uploadField.setFieldType(FieldType.FILE);
 		uploadField.setImmediate(false);
@@ -111,7 +107,12 @@ public class UploadDataUI extends VerticalLayout {
 	public void theButtonClick(Button.ClickEvent event) {
     	
 		String dataDescription 	= 	(String) dataArea.getValue();
-		if(uploadType.getValue().toString().equalsIgnoreCase("Upload from your Desktop")) {
+		String fileType 		= 	(String) fileCombo.getValue();
+		File dataFile 			= 	(File) uploadField.getValue();
+
+		parseInit(dataFile, null, fileType, dataDescription);
+		dataFile.delete();
+		/*if(uploadType.getValue().toString().equalsIgnoreCase("Upload from your Desktop")) {
     		
     		String fileType 		= 	(String) fileCombo.getValue();
     		File dataFile 			= 	(File) uploadField.getValue();
@@ -127,7 +128,7 @@ public class UploadDataUI extends VerticalLayout {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    	}
+    	}*/
     }
     
 	protected void parseInit(File dataFile, File annotFile, String fileType, String dataDescription) {
