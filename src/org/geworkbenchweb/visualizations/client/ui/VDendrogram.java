bb1418a264ba91bc85c2c5ebb69e960965e6c6ce
Zhou Ji <zji@c2b2.columbia.edu>
2013-02-19 16:12:54 +0000

    use the actually necessary height of dendrogram instead of a preset value
    git-svn-id: https://ncisvn.nci.nih.gov/svn/geworkbench/trunk/geworkbench-web@10332 8b29bb6e-589a-435a-9a12-3ab907be3462

--- src/org/geworkbenchweb/visualizations/client/ui/VDendrogram.java
+++ src/org/geworkbenchweb/visualizations/client/ui/VDendrogram.java
@@ -1,5 +1,8 @@
 package org.geworkbenchweb.visualizations.client.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.canvas.dom.client.CssColor;
@@ -108,22 +111,30 @@ public class VDendrogram extends Widget implements Paintable {
 		Canvas arrayDendrogramCanvas = Canvas.createIfSupported();
 		arrayDendrogramCanvas.setCoordinateSpaceWidth(canvasWidth);
 		Context2d arrayDendrogramContext = arrayDendrogramCanvas.getContext2d();
-		int arrayClusterHeight = 400; // TODO strategy to be decided
-		arrayDendrogramCanvas.setCoordinateSpaceHeight(arrayClusterHeight); //(int)midPoint.height);
-		arrayDendrogramContext.transform(1, 0, 0, -1, 0, arrayClusterHeight); // flip upside down
-//		arrayDendrogramContext.setStrokeStyle(CssColor.make(0,128,0));
-
-		final char[] clusters = arrayCluster.toCharArray();
-		String v = verifyClusterString(0, clusters.length-1, clusters, 0);
-		if(v!=null) {
-			arrayDendrogramContext.setFont("20px");
-			arrayDendrogramContext.fillText("ill-formed cluster string:"+v, 10, 30);
-		}
-		arrayDendrogramContext.beginPath();
+		
 		index = 0; // this must be reset to start reading the cluster string
-		MidPoint midPoint = drawBracket(0, clusters.length-1, clusters, arrayDendrogramContext); // ignore the top level return value?
-//		arrayDendrogramCanvas.setCoordinateSpaceHeight(arrayClusterHeight); //(int)midPoint.height);
-//		arrayDendrogramContext.transform(1, 0, 0, -1, 0, midPoint.height); // flip upside down
+		List<Double> bracketCoordinates = new ArrayList<Double>();
+		final char[] clusters = arrayCluster.toCharArray();
+		MidPoint midPoint = prepareBrackets(0, clusters.length-1, clusters, bracketCoordinates); // ignore the top level return value?
+
+		final int EXTA_SPACE = 5;
+		int arrayClusterHeight = (int)midPoint.height + EXTA_SPACE;
+		arrayDendrogramCanvas.setCoordinateSpaceHeight(arrayClusterHeight);
+		arrayDendrogramContext.transform(1, 0, 0, -1, 0, arrayClusterHeight); // flip upside down
+
+		// draw brackets
+		arrayDendrogramContext.beginPath();
+		for(int i=0; i<bracketCoordinates.size(); i+=5) {
+			double x1 = bracketCoordinates.get(i);
+			double x2 = bracketCoordinates.get(i+1);
+			double y1 = bracketCoordinates.get(i+2);
+			double y = bracketCoordinates.get(i+3);
+			double y2 = bracketCoordinates.get(i+4);
+			arrayDendrogramContext.moveTo(x1, y1);
+			arrayDendrogramContext.lineTo(x1, y);
+			arrayDendrogramContext.lineTo(x2, y);
+			arrayDendrogramContext.lineTo(x2, y2);
+		}
 		arrayDendrogramContext.stroke();
 
 		// <div><canvas id=array_dendrogram></canvas><canvas id=array_heatmap></canvas><canvas id=array_labels></canvas></div>
@@ -151,33 +162,39 @@ public class VDendrogram extends Widget implements Paintable {
 	static double x0 = 0;
 	static double deltaX = 10;
 	
-	/* the first and last characters of String cluster must be "(" and ")", respectively. */
-	/* just to be efficient, we assume it is always well formed. if necessary we can use separate step to verify this */ 
-	private MidPoint drawBracket(int leftMid, int rightMid, final char[] clusters, final Context2d context) {
-		if(rightMid-leftMid==1) { // it never should be shorter
+	/**
+	 * Prepare the collection of three points coordinates to draw the brackets,
+	 * and calculate the height so the container know the size of the resulted dendrogram.
+	 * 
+	 * precondition: clusters[left]=='(', clusters[right]=')'
+	 */
+	private MidPoint prepareBrackets(int left, int right, final char[] clusters, final List<Double> coordinates) {
+		if(right-left==1) { 
 			MidPoint m = new MidPoint(x0+(index+0.5)*deltaX, 0);
 			index++;
 			return m;
 		}
-		
+
 		// the general case that includes child clusters, and they must be two.
-		int split = split(leftMid + 1, rightMid -1, clusters);
+		int split = split(left + 1, right -1, clusters);
 
 		// by now, [0, index) is the left child; [index, length-1] is the right child
-		MidPoint left = drawBracket(leftMid+1, split-1, clusters, context);
-		MidPoint right = drawBracket(split, rightMid-1, clusters, context);
-		double height = Math.max(left.height, right.height) + deltaH;
-		double x1 = 0.5+(int)left.mid; // trick to create crisp line if width 1
-		double x2 = 0.5+(int)right.mid;
+		MidPoint leftMidPoint = prepareBrackets(left+1, split-1, clusters, coordinates);
+		MidPoint rightMidPoint = prepareBrackets(split, right-1, clusters, coordinates);
+		double height = Math.max(leftMidPoint.height, rightMidPoint.height) + deltaH;
+		double x1 = 0.5+(int)leftMidPoint.mid; // trick to create crisp line if width 1
+		double x2 = 0.5+(int)rightMidPoint.mid;
 		double y = 0.5+(int)height;
-		context.moveTo(x1, left.height);
-		context.lineTo(x1, y);
-		context.lineTo(x2, y);
-		context.lineTo(x2, right.height);
+		// five coordinate values needed to draw a bracket
+		coordinates.add(x1);
+		coordinates.add(x2);
+		coordinates.add(leftMidPoint.height);
+		coordinates.add(y);
+		coordinates.add(rightMidPoint.height);
 	
-		return new MidPoint(0.5*(left.mid+right.mid), height);
+		return new MidPoint(0.5*(leftMidPoint.mid+rightMidPoint.mid), height);
 	}
-
+	
 	// assume [begin, end] contains two nodes, returns the starting position of the second one
 	static private int split(int begin, int end, char[] clusters) {
 		int split = begin;
@@ -191,19 +208,6 @@ public class VDendrogram extends Widget implements Paintable {
 			split++;
 		} while (count>0 || split==end);
 		return split;
-	}
-	
-	/* This is mainly useful for debug purpose. return null if the string is well-formed 'cluster string'; otherwise a error message. */
-	static private String verifyClusterString(int begin, int end, char[] clusters, int c) {
-		if(clusters[begin]!='(') return "first letter in ("+begin+","+end+") is "+clusters[begin];
-		if(clusters[end]!=')') return "last letter  in ("+begin+","+end+") is "+clusters[end];
-		if(begin+1==end) return null;
-		int split = split(begin+1, end-1, clusters);
-		String s = verifyClusterString(begin+1, split-1, clusters, c+1);
-		if(s!=null) return c+" "+s;
-		s = verifyClusterString(split, end-1, clusters, c+1);
-		if(s!=null) return c+" "+s;
-		else return null;
 	}
 
 }
