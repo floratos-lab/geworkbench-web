8ac149807a33135a75a52562dacdbe89aa39a9d5
Min You <my2248@c2b2.columbia.edu>
2013-12-17 12:07:48 -0500

    change maven build

---
+++ src/main/java/org/geworkbenchweb/layout/AnnotationTabSheet.java
@@ -0,0 +1,179 @@
+/**
+ * 
+ */
+package org.geworkbenchweb.layout;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.geworkbenchweb.pojos.Comment;
+import org.geworkbenchweb.pojos.DataHistory;
+import org.geworkbenchweb.pojos.ExperimentInfo;
+import org.vaadin.appfoundation.persistence.facade.FacadeFactory;
+import org.vaadin.appfoundation.persistence.data.AbstractPojo;
+
+import com.vaadin.event.ShortcutAction.KeyCode;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.HorizontalSplitPanel;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.Reindeer;
+
+/**
+ * @author zji
+ * @version $Id$
+ *
+ */
+/* ATTENTION: please be aware of our over-use of the word 'annotation'!
+ * 'annotation' here refers to the additional information of the dataset, e.g. user comment, history, experimental info, etc.,
+ * NOT the annotation for the microaaray dataset, e.g. the AffyMatrix annotation*/
+public class AnnotationTabSheet extends TabSheet {
+
+	private static final long serialVersionUID = -727899117302032942L;
+
+	AnnotationTabSheet(final Long dataSetId) {
+
+		HorizontalSplitPanel dLayout 	=  	new HorizontalSplitPanel();
+		dLayout.setSplitPosition(60);
+		dLayout.setSizeFull();
+		dLayout.setImmediate(true);
+		dLayout.setStyleName(Reindeer.SPLITPANEL_SMALL);
+		dLayout.setLocked(true);
+		
+		final VerticalLayout commentsLayout = new VerticalLayout();
+		commentsLayout.setImmediate(true);
+		commentsLayout.setMargin(true);
+		commentsLayout.setSpacing(true);
+		commentsLayout.setSizeUndefined();
+		
+		Label cHeading 		=	new Label("User Comments:");
+		cHeading.setStyleName(Reindeer.LABEL_H2);
+		cHeading.setContentMode(Label.CONTENT_PREFORMATTED);
+		commentsLayout.addComponent(cHeading);
+		
+		Map<String, Object> params 		= 	new HashMap<String, Object>();
+		params.put("parent", dataSetId);
+
+		List<AbstractPojo> comments =  FacadeFactory.getFacade().list("Select p from Comment as p where p.parent =:parent", params);
+		if(comments.size() != 0){
+			for(int i=0;i<comments.size();i++) {
+				java.sql.Date date = ((Comment) comments.get(i)).getDate();
+				Label comment = new Label(date.toString()+
+						" - " +
+						((Comment) comments.get(i)).getComment());
+				commentsLayout.addComponent(comment);
+			}
+		}
+
+		dLayout.setFirstComponent(commentsLayout);
+		
+		VerticalLayout commentArea = new VerticalLayout();
+		commentArea.setImmediate(true);
+		commentArea.setMargin(true);
+		commentArea.setSpacing(true);
+		
+		Label commentHead 		=	new Label("Enter new comment here:");
+		commentHead.setStyleName(Reindeer.LABEL_H2);
+		commentHead.setContentMode(Label.CONTENT_PREFORMATTED);
+		final TextArea dataArea = 	new TextArea();
+		dataArea.setRows(6);
+		dataArea.setWidth("100%");
+		Button submitComment	=	new Button("Add Comment", new Button.ClickListener() {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				if(!dataArea.getValue().toString().isEmpty()) {
+					java.sql.Date date 	=	new java.sql.Date(System.currentTimeMillis());
+					
+					Comment c = new Comment();
+					c.setParent(dataSetId);
+					c.setComment(dataArea.getValue().toString());
+					c.setDate(date);
+					FacadeFactory.getFacade().store(c);
+					
+					Label comment = new Label(date.toString()+
+							" - " +
+							dataArea.getValue().toString());
+					commentsLayout.addComponent(comment);
+					dataArea.setValue("");
+				}
+			}
+		});
+		submitComment.setClickShortcut(KeyCode.ENTER);
+		commentArea.addComponent(commentHead);
+		commentArea.setComponentAlignment(commentHead, Alignment.MIDDLE_LEFT);
+		commentArea.addComponent(dataArea);
+		commentArea.setComponentAlignment(dataArea, Alignment.MIDDLE_LEFT);
+		commentArea.addComponent(submitComment);
+		commentArea.setComponentAlignment(submitComment, Alignment.MIDDLE_LEFT);
+		dLayout.setSecondComponent(commentArea);
+		
+		HorizontalSplitPanel infoSplit 	=  	new HorizontalSplitPanel();
+		infoSplit.setSplitPosition(50);
+		infoSplit.setSizeFull();
+		infoSplit.setImmediate(true);
+		infoSplit.setStyleName(Reindeer.SPLITPANEL_SMALL);
+		infoSplit.setLocked(true);
+				
+		VerticalLayout dataHistory 	= 	new VerticalLayout();
+		VerticalLayout expInfo		=	new VerticalLayout();
+		
+		dataHistory.setSizeUndefined();
+		dataHistory.setMargin(true);
+		dataHistory.setSpacing(true);
+		dataHistory.setImmediate(true);
+
+		Label historyHead 		=	new Label("Data History:");
+		historyHead.setStyleName(Reindeer.LABEL_H2);
+		historyHead.setContentMode(Label.CONTENT_PREFORMATTED);
+		dataHistory.addComponent(historyHead);
+		
+		Map<String, Object> eParams 		= 	new HashMap<String, Object>();
+		eParams.put("parent", dataSetId);
+
+		List<AbstractPojo> histories =  FacadeFactory.getFacade().list("Select p from DataHistory as p where p.parent =:parent", eParams);
+		for(int i=0; i<histories.size(); i++) {
+			DataHistory dH = (DataHistory) histories.get(i);
+			Label d = new Label(dH.getData());
+			d.setContentMode(Label.CONTENT_PREFORMATTED);
+			dataHistory.addComponent(d);
+		}
+		
+		expInfo.setSizeUndefined();
+		expInfo.setMargin(true);
+		expInfo.setSpacing(true);
+		
+		Label infoHead 		=	new Label("Experiment Information:");
+		infoHead.setStyleName(Reindeer.LABEL_H2);
+		infoHead.setContentMode(Label.CONTENT_PREFORMATTED);
+		expInfo.addComponent(infoHead);
+		
+		Map<String, Object> iParams 		= 	new HashMap<String, Object>();
+		iParams.put("parent", dataSetId);
+
+		List<AbstractPojo> info =  FacadeFactory.getFacade().list("Select p from ExperimentInfo as p where p.parent =:parent", iParams);
+		for(int i=0; i<info.size(); i++) {
+			ExperimentInfo eI = (ExperimentInfo) info.get(i);
+			Label d = new Label(eI.getInfo());
+			d.setContentMode(Label.CONTENT_PREFORMATTED);
+			expInfo.addComponent(d);
+		}
+	
+		infoSplit.setFirstComponent(dataHistory);
+		infoSplit.setSecondComponent(expInfo);
+
+		this.setStyleName(Reindeer.TABSHEET_SMALL);
+		this.setSizeFull();
+		this.setImmediate(true);
+	
+		this.addTab(infoSplit, "Data Information");
+		this.addTab(dLayout, "User Comments");			
+	}
+}
---
+++ src/main/java/org/geworkbenchweb/plugins/NetworkViewer.java
@@ -0,0 +1,286 @@
+package org.geworkbenchweb.plugins;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.geworkbench.bison.datastructure.biocollections.AdjacencyMatrix;
+import org.geworkbench.bison.datastructure.biocollections.AdjacencyMatrix.NodeType;
+import org.geworkbenchweb.GeworkbenchRoot;
+import org.geworkbenchweb.utils.UserDirUtils;
+import org.geworkbenchweb.visualizations.Cytoscape;
+
+import com.vaadin.ui.Label;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.MenuBar.Command;
+import com.vaadin.ui.MenuBar.MenuItem;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.VerticalLayout;
+
+import de.steinwedel.vaadin.MessageBox;
+import de.steinwedel.vaadin.MessageBox.ButtonType;
+
+/* this used to be AracneResultsUI, but is in fact used by both ARACNe result and CNKB result. */
+public class NetworkViewer extends VerticalLayout implements Visualizer {
+
+	private static final long serialVersionUID = 1L;
+
+	private Log log = LogFactory.getLog(NetworkViewer.class);
+
+	private static final String LIMIT_CYTOSCAPE_OBJECTS = "limit.cytoscape.objects";
+	private static final int DEFAULT_LIMIT_CYTOSCAPE_OBJECTS = 5000;
+	private static int limit_num = 0;
+	
+	// TODO final is remove due to jdk compile problem, need to fix the real problem
+	private AdjacencyMatrix adjMatrix; 	
+
+	final private Long datasetId;
+	
+	public NetworkViewer(Long dataSetId) {
+		datasetId = dataSetId;
+		if(dataSetId==null) {
+			adjMatrix = null;
+			return;
+		}
+
+		Object object = null;
+		try {
+			object = UserDirUtils.deserializeResultSet(dataSetId);
+		} catch (FileNotFoundException e) { 
+			// TODO pending node should be designed and implemented explicitly as so, eventually
+			// let's make a naive assumption for now that "file not found" means pending computation
+			addComponent(new Label("Pending computation - ID "+ dataSetId));
+			adjMatrix = null;
+			return;
+		} catch (IOException e) {
+			addComponent(new Label("Result (ID "+ dataSetId+ ") not available due to "+e));
+			adjMatrix = null;
+			return;
+		} catch (ClassNotFoundException e) {
+			addComponent(new Label("Result (ID "+ dataSetId+ ") not available due to "+e));
+			adjMatrix = null;
+			return;
+		}
+		if(! (object instanceof AdjacencyMatrix)) {
+			String type = null;
+			if(object!=null) type = object.getClass().getName();
+			addComponent(new Label("Result (ID "+ dataSetId+ ") has wrong type: "+type));
+			adjMatrix = null;
+			return;
+		}
+		adjMatrix = (AdjacencyMatrix) object;
+
+		setImmediate(true);
+		setSizeFull();		 
+		getLimitCytoscapeObjectsNum();
+
+		// the following code used to be attach - which is not really necessary and is not consistent with other plug-ins
+		int edgeNumber = adjMatrix.getConnectionNo();
+		int nodeNumber = adjMatrix.getNodeNumber();
+		if ((edgeNumber + nodeNumber) > limit_num) {
+			String theMessage = "This network has "
+					+ nodeNumber
+					+ " nodes and "
+					+ edgeNumber
+					+ " edges, which may be too large to display in Cytoscape. \nAn alternate, text view is available instead.";
+
+			MessageBox mb = new MessageBox(getWindow(), "Warning", null,
+					theMessage, new MessageBox.ButtonConfig(
+							MessageBox.ButtonType.CUSTOM1, "View as text"),
+					new MessageBox.ButtonConfig(MessageBox.ButtonType.CUSTOM2,
+							"View in Cytoscape"));
+			mb.show(new MessageBox.EventListener() {
+
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public void buttonClicked(ButtonType buttonType) {
+					if (buttonType == ButtonType.CUSTOM1)  
+						viewAsText();					    
+					else 
+						viewAsCytoscape();
+				}
+			});
+		} else
+		{
+			viewAsCytoscape();
+		}
+	}
+	
+	static private String getExportName(AdjacencyMatrix.Node node) {
+		if (node.type == NodeType.MARKER) {
+			return node.marker.getLabel();
+		} else if (node.type == NodeType.GENE_SYMBOL) {
+			return node.stringId;
+		} else if (node.type == NodeType.STRING) {
+			return node.stringId;
+		} else {
+			return "unknown";
+		}
+	}
+
+	private void viewAsText()
+	{
+		TextArea area = new TextArea();
+		area.setSizeFull();
+	
+		area.setWordwrap(false);
+		 
+		area.setImmediate(true);
+		int edgeNumber = adjMatrix.getConnectionNo();
+		int nodeNumber = adjMatrix.getNodeNumber();
+		StringBuffer sb = new StringBuffer("This network has "
+				+ nodeNumber + " nodes and " + edgeNumber
+				+ " edges: \n\n");
+        
+       
+		for (AdjacencyMatrix.Node node1 : adjMatrix.getNodes()) {
+			
+			sb.append(getExportName(node1) + "\t");
+
+			for (AdjacencyMatrix.Edge edge : adjMatrix
+					.getEdges(node1)) {
+				sb.append(getExportName(edge.node2)
+						+ "\t" + edge.info.value + "\t");
+			}
+			sb.append("\n");
+			
+			 
+		}
+		area.setValue(sb.toString());
+		 
+		 
+		addComponent(area);
+		 
+		
+	}
+
+	private void viewAsCytoscape()
+	{
+		 
+		/* Preparing data for cytoscape */
+		ArrayList<String> nodes = new ArrayList<String>();
+		ArrayList<String> edges = new ArrayList<String>();
+
+		for (int i = 0; i < adjMatrix.getEdges()
+				.size(); i++) {
+			String id1;
+			String label1;
+			String id2;
+			String label2;
+			if (adjMatrix.getEdges().get(i).node1.type
+					.equals(NodeType.MARKER)) {
+				id1 = adjMatrix.getEdges().get(i).node1.marker
+						.getLabel();
+				label1 = adjMatrix.getEdges()
+						.get(i).node1.marker.getGeneName();
+				if (label1.equals("---"))
+					label1 = id1;
+				id2 = adjMatrix.getEdges().get(i).node2.marker
+						.getLabel();
+				label2 = adjMatrix.getEdges()
+						.get(i).node2.marker.getGeneName();
+				if (label2.equals("---"))
+					label2 = id2;
+
+			} else {
+				id1 = adjMatrix.getEdges().get(i).node1
+						.getStringId();
+				label1 = id1;
+				id2 = adjMatrix.getEdges().get(i).node2
+						.getStringId();
+				label2 = id2;
+			}
+
+			String edge = id1 + "," + id2;
+
+			String node1 = id1 + "," + label1 + ",0";
+			String node2 = id2 + "," + label2 + ",0";
+
+			if (edges.isEmpty()) {
+				edges.add(edge);
+			} else if (!edges.contains(edge)) {
+				edges.add(edge);
+			}
+
+			if (nodes.isEmpty()) {
+				nodes.add(node1);
+				nodes.add(node2);
+			} else {
+				if (!nodes.contains(node1)) {
+					nodes.add(node1);
+				}
+				if (!nodes.contains(node2)) {
+					nodes.add(node2);
+				}
+			}
+		}
+
+		final Cytoscape cy = new Cytoscape();
+		cy.setImmediate(true);
+		cy.setSizeFull();
+
+		String[] nodeArray = new String[nodes.size()];
+		String[] edgeArray = new String[edges.size()];
+
+		nodeArray = nodes.toArray(nodeArray);
+		edgeArray = edges.toArray(edgeArray);
+
+		cy.setNodes(nodeArray);
+		cy.setEdges(edgeArray);		 
+	 
+       	Command layoutCommand = new Command() {
+
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void menuSelected(MenuItem selectedItem) {
+				cy.setLayout(selectedItem.getText());
+				cy.requestRepaint();
+			}
+       		
+       	};
+		MenuBar toolBar =  new MenuBar();
+		toolBar.setStyleName("transparent");
+		MenuItem layoutNames = toolBar.addItem("Layout managers", null);
+		layoutNames.addItem("ForceDirected", layoutCommand);
+       	layoutNames.addItem("Circle", layoutCommand);
+       	layoutNames.addItem("Radial", layoutCommand);
+       	layoutNames.addItem("Tree", layoutCommand);
+       	layoutNames.addItem("CompoundSpringEmbedder", layoutCommand);
+
+       	layoutNames.setStyleName("plugin");
+		
+		this.setSpacing(true);
+		this.addComponent(toolBar);
+		this.addComponent(cy);
+		this.setExpandRatio(cy, 1);
+	}
+	
+	private void getLimitCytoscapeObjectsNum() {
+		if (limit_num == 0) {
+			try {
+				limit_num = Integer.parseInt(GeworkbenchRoot.getAppProperty(LIMIT_CYTOSCAPE_OBJECTS));
+			} catch (NumberFormatException e) {
+				log.warn("limit cytoscape objects value is not set properly.");
+			}
+			if (limit_num == 0)
+				limit_num = DEFAULT_LIMIT_CYTOSCAPE_OBJECTS;
+
+		}
+
+	}
+
+	@Override
+	public PluginEntry getPluginEntry() {
+		return GeworkbenchRoot.getPluginRegistry().getVisualizerPluginEntry(this.getClass());
+	}
+
+	@Override
+	public Long getDatasetId() {
+		return datasetId;
+	}
+
+}
