e6a4014be7b8fcc5c454efcfafd5ebcddff90d97
Zhou Ji <zji@c2b2.columbia.edu>
2014-01-09 13:40:12 -0500

    minor cleaninp-up in genspace code

--- src/main/java/org/geworkbench/components/genspace/ui/chat/ChatWindow.java
+++
@@ -1,9 +0,0 @@
-package org.geworkbench.components.genspace.ui.chat;
-
-import java.io.Serializable;
-
-public class ChatWindow {
-	public enum messageTypes {
-		WORKFLOW, SCREEN_REQUEST, SCREEN_HANDSHAKE, CHAT, SCREEN_TX_END, SCREEN_RX_END
-	};
-}
--- src/main/java/org/geworkbenchweb/genspace/ui/chat/ChatWindow.java
+++ src/main/java/org/geworkbenchweb/genspace/ui/chat/ChatWindow.java
@@ -9,20 +9,19 @@ package org.geworkbenchweb.genspace.ui.chat;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
 
 import org.geworkbench.components.genspace.server.stubs.Workflow;
-import org.geworkbench.components.genspace.ui.chat.ChatWindow.messageTypes;
 import org.geworkbenchweb.genspace.ui.component.GenSpaceLogin_1;
 import org.geworkbenchweb.genspace.ui.component.WorkflowVisualizationPanel;
 import org.geworkbenchweb.genspace.wrapper.WorkflowWrapper;
-
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
 import org.vaadin.addon.borderlayout.BorderLayout;
 import org.vaadin.artur.icepush.ICEPush;
 
+import com.vaadin.event.Action;
+import com.vaadin.event.ShortcutAction;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.MenuBar.Command;
@@ -31,10 +30,13 @@ import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
-import com.vaadin.event.Action;
-import com.vaadin.event.ShortcutAction;
 
 public class ChatWindow extends Window implements Action.Handler{
+	
+	enum messageTypes {
+		WORKFLOW, SCREEN_REQUEST, SCREEN_HANDSHAKE, CHAT, SCREEN_TX_END, SCREEN_RX_END
+	};
+	
 	/**
 	 * 
 	 */
@@ -47,9 +49,6 @@ public class ChatWindow extends Window implements Action.Handler{
 	};
 
 	private lastChatter last = lastChatter.NONE;
-	//private ScreenSharingReceiver screenReceiver;
-	private HashMap<Integer, Integer> tileHashes = new HashMap<Integer, Integer>();
-	private final static int TILE_SIZE = 32;
 	private GenSpaceLogin_1 login;
 	private ICEPush pusher = new ICEPush();
 
@@ -304,10 +303,6 @@ public class ChatWindow extends Window implements Action.Handler{
 		this.getParent().removeWindow(this);
 	}
 
-	//private ScreenSharePublisher screenPublisher;
-	//Timer screenSendTimer;
-	private final int SCREEN_FPS = 300;
-
 	/**
 	 * Create a timer to automatically send the screen
 	 * 
@@ -326,122 +321,6 @@ public class ChatWindow extends Window implements Action.Handler{
 			};
 		});
 		screenSendTimer.start();*/
-	}
-
-	private boolean amSharing = false;
-
-	/**
-	 * Either start sending or stop sending the screen
-	 * 
-	 * @param evt
-	 */
-	private void mnuShareScreenActionPerformed(java.awt.event.ActionEvent evt) {
-		/*if (amSharing) {
-			// Tell the other guy to stop
-
-			screenPublisher.stop();
-			Message m = new Message();
-			m.setBody("hidden");
-			m.setProperty("specialType", messageTypes.SCREEN_RX_END);
-			try {
-				chat.sendMessage(m);
-			} catch (XMPPException e) {
-				GenSpace.logger.warn("Error",e);
-			}
-			screenSendTimer.stop();
-			amSharing = false;
-		} else if (sharingScreen) {
-			JDialog warning = new JDialog(this, "Can't start screen share",
-					true);
-			warning.setVisible(true);
-		} else {
-			amSharing = true;
-			sharingScreen = true;
-			mnuShareScreen.setText("Stop Sharing Screen");
-			try {
-				Message m = new Message();
-				m.setBody("SEND TO ME");
-				m.setProperty("specialType", messageTypes.SCREEN_REQUEST);
-				chat.sendMessage(m);
-			} catch (XMPPException e) {
-				GenSpace.logger.warn("Error",e);
-			}
-		}*/
-
-	}
-
-	/**
-	 * Send a single screen update message
-	 */
-	private void sendScreenUpdate() {
-		/*Robot r;
-		try {
-			r = new Robot();
-			BufferedImage n = r.createScreenCapture(GUIFramework.getFrame()
-					.getBounds());
-			// BufferedImage n = r.createScreenCapture(new
-			// Rectangle(0,0,10,10));
-			int imgNum;
-			HashMap<Integer, byte[]> output = new HashMap<Integer, byte[]>();
-
-			// Find updated tiles
-			int numTiles = 0;
-			for (int j = 0; j < n.getWidth(null) && numTiles < 100; j += TILE_SIZE) {
-				for (int k = 0; k < n.getHeight(null) && numTiles < 100; k += TILE_SIZE) {
-					imgNum = n.getWidth(null) * k + j;
-
-					Rectangle this_tile = new Rectangle(j, k,
-							(j + TILE_SIZE > n.getWidth(null) ? n
-									.getWidth(null) - j : TILE_SIZE),
-							(k + TILE_SIZE > n.getHeight(null) ? n
-									.getHeight(null) - k : TILE_SIZE));
-					WritableRaster tile = (WritableRaster) n.getData(this_tile);
-
-					Raster tile_for_code = n.getData(this_tile);
-					int[] ints = null;
-					ints = tile_for_code.getPixels(tile_for_code.getMinX(),
-							tile_for_code.getMinY(), tile_for_code.getWidth(),
-							tile_for_code.getHeight(), ints);
-					int code = 0;
-					for (int l = 0; l < ints.length; l++)
-						code += ints[l] % ints.length;
-					if (!tileHashes.containsKey(imgNum)
-							|| !tileHashes.get(imgNum).equals(code)) {
-						tileHashes.put(imgNum, code);
-						WritableRaster child = tile.createWritableChild(
-								tile.getMinX(), tile.getMinY(),
-								tile.getWidth(), tile.getHeight(), 0, 0, null);
-
-						BufferedImage child_img = new BufferedImage(
-								n.getColorModel(), child, false, null);
-						ByteArrayOutputStream bs = new ByteArrayOutputStream();
-						Jimi.putImage("image/png", child_img, bs);
-
-						numTiles++;
-						output.put(imgNum, bs.toByteArray());
-					}
-
-				}
-			}
-
-			if (!output.isEmpty()) {
-				// Assemble the message
-				HashMap<String, Object> m = new HashMap<String, Object>();
-				m.put("width", n.getWidth());
-				m.put("height", n.getHeight());
-				m.put("indices", output.keySet().toArray());
-				for (Integer tile : output.keySet())
-					m.put("img" + tile, output.get(tile));
-				screenPublisher.sendMessage(m);
-			}
-
-		} catch (AWTException e) {
-			//GenSpace.logger.warn("Error",e);
-			GenSpaceServerFactory.logger.warn("Error", e);
-		} catch (JimiException e) {
-			//GenSpace.logger.warn("Error",e);
-			GenSpaceServerFactory.logger.warn("Error", e);
-		}*/
 	}
 
 	/**
