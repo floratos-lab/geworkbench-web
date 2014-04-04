1b3cb0c09096816803c88ff117e6a7dcb504c100
mw2518 <mw2518@columbia.edu>
2014-04-04 15:55:56 -0400

    #3746: saved dataset with unique name

--- src/main/java/org/geworkbenchweb/plugins/uploaddata/DataFileReceiver.java
+++ src/main/java/org/geworkbenchweb/plugins/uploaddata/DataFileReceiver.java
@@ -3,9 +3,16 @@ package org.geworkbenchweb.plugins.uploaddata;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.geworkbenchweb.GeworkbenchRoot;
+import org.geworkbenchweb.pojos.DataSet;
 import org.vaadin.appfoundation.authentication.SessionHandler;
+import org.vaadin.appfoundation.persistence.facade.FacadeFactory;
 
 import com.vaadin.ui.Upload.Receiver;
 
@@ -13,6 +20,7 @@ import com.vaadin.ui.Upload.Receiver;
 class DataFileReceiver implements Receiver {
 
 	private static final long serialVersionUID = 3374720899370397672L;
+	private static Log log = LogFactory.getLog(DataFileReceiver.class);
 
 	private File file = null;
 
@@ -32,6 +40,7 @@ class DataFileReceiver implements Receiver {
 				+ System.getProperty("file.separator") + subdirectoryName;
 		if (!new File(dir).exists())
 			new File(dir).mkdirs();
+		filename = getUniqueFileName(filename);
 		file = new File(dir, filename);
 		try {
 			// Open the file for writing.
@@ -45,6 +54,38 @@ class DataFileReceiver implements Receiver {
 		return fos;
 	}
 
+	private String getUniqueFileName(String filename){
+		int suffixId = filename.lastIndexOf('.');
+		String mainpart = filename, suffix = "";
+		if(suffixId >= 0){
+			mainpart = filename.substring(0, suffixId);
+			suffix = filename.substring(suffixId);
+		}
+
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("owner", SessionHandler.get().getId());
+		params.put("name", mainpart+"%"+suffix);
+		List<DataSet> datasets = FacadeFactory.getFacade().list(
+				"select d from DataSet as d where d.owner=:owner and d.name like :name", params);
+		if(datasets.size()==0) return filename;
+
+		int maxseq = 0;
+		for(DataSet ds : datasets){
+			int start = suffixId + 1;//(
+			int end = ds.getName().lastIndexOf(suffix)-1;//).
+			if(start<end){
+				String number = ds.getName().substring(start, end);
+				try{
+					maxseq = Math.max(maxseq, Integer.parseInt(number));
+				}catch(NumberFormatException e){
+					log.warn("dataset name number parsing error: "+number);
+				}
+			}
+		}
+		filename = mainpart+"("+(++maxseq)+")"+suffix;
+		return filename;
+	}
+	
 	public File getFile() {
 		return file;
 	}
