84e3cb2565c822c20c896b8e7b14db02fd8e2c51
Zhou Ji <zj2122@columbia.edu>
2016-02-17 17:10:29 -0500

    put the test data in the server side Java code

--- src/main/java/org/geworkbenchweb/visualizations/CitrusDiagram.java
+++ src/main/java/org/geworkbenchweb/visualizations/CitrusDiagram.java
@@ -8,14 +8,70 @@ import com.vaadin.ui.AbstractComponent;
 public class CitrusDiagram extends AbstractComponent {
 
 	private static final long serialVersionUID = 2073659992756579843L;
-	final private String[] alteration;
-	final private Float[] nes;
+	final private String[] alteration, samples, presence;
+	final private Integer[] preppi, cindy;
+	final private String[] pvalue;
+	final private String[] nes;
 
 	public CitrusDiagram() {
-		alteration = new String[] { "1100", "0011", "1001" };
-		nes = new Float[10];
-		for (int i = 0; i < nes.length; i++)
-			nes[i] = (float) i;
+    	/* create random test data */
+		int n = 30, m = 100;
+		String[] colorKeys = {"UMT", "DMT", "AMP", "DEL", "SNV", "GFU"};
+		alteration = new String[n];
+		for(int i=0; i<n; i++) {
+    		int randomIndex = (int)( Math.random() * colorKeys.length );
+            String geneSymbol = "";
+            for(int j=0; j<4; j++) {
+                char c = (char)((Math.random()*26)+'A');
+                geneSymbol += c;
+            } 
+    		alteration[i] = colorKeys[randomIndex]+"_"+geneSymbol;
+		};
+
+        samples = new String[m];
+        for(int j=0; j<m; j++) {
+            samples[j] = "";
+            for(int k=0; k<10; k++) {
+                char c = (char)('A' + Math.random()*26 );
+                samples[j] += c;
+            } 
+        }
+
+        presence = new String[n];
+    	for(int i=0; i<n; i++) {
+    		presence[i] = "";
+    		double r = Math.random();
+    		for(int j=0; j<m; j++) {
+    			char p = '_';
+    			double x = Math.random();
+    			if(x>r)
+    				p = '1';
+    			else
+    				p = '0';
+    			presence[i] += p;
+    		}
+    	}
+
+    	preppi = new Integer[n];
+        for(int i=0; i<n; i++) {
+            preppi[i] = (int)(Math.random()*2);
+        }
+
+        cindy = new Integer[n];
+        for(int i=0; i<n; i++) {
+            cindy[i] = (int)(Math.random()*2);
+        }
+        
+        pvalue = new String[n];
+        for(int i=0; i<n; i++) {
+            pvalue[i] = String.valueOf( Math.random() );
+        }
+
+		nes = new String[m];
+		for (int i = 0; i < m; i++) {
+			nes[i] = String.valueOf( Math.random()*2.-1. );
+		}
+		/* end of creating random test data */
 	}
 
 	@Override
@@ -23,6 +79,11 @@ public class CitrusDiagram extends AbstractComponent {
 		super.paintContent(target);
 
 		target.addAttribute("alteration", alteration);
+		target.addAttribute("samples", samples);
+		target.addAttribute("presence", presence);
+		target.addAttribute("preppi", preppi);
+		target.addAttribute("cindy", cindy);
+		target.addAttribute("pvalue", pvalue);
 		target.addAttribute("nes", nes);
 	}
 }
--- src/main/java/org/geworkbenchweb/visualizations/client/ui/VCitrusDiagram.java
+++ src/main/java/org/geworkbenchweb/visualizations/client/ui/VCitrusDiagram.java
@@ -1,6 +1,8 @@
 package org.geworkbenchweb.visualizations.client.ui;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArrayInteger;
+import com.google.gwt.core.client.JsArrayNumber;
 import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.DOM;
@@ -26,14 +28,39 @@ public class VCitrusDiagram extends Widget implements Paintable {
 		placeholder.setId(uidl.getId());
 
 		String[] alteration = uidl.getStringArrayAttribute("alteration");
+		String[] samples = uidl.getStringArrayAttribute("samples");
+		String[] presence = uidl.getStringArrayAttribute("presence");
+		int[] preppi = uidl.getIntArrayAttribute("preppi");
+		int[] cindy = uidl.getIntArrayAttribute("cindy");
+		String[] pvalue = uidl.getStringArrayAttribute("pvalue");
 		String[] nes = uidl.getStringArrayAttribute("nes");
 
-		createInstance(placeholder.getId(), wrapArray(alteration), wrapArray(nes));
+		createInstance(placeholder.getId(), wrapArray(alteration), wrapArray(samples),
+				wrapArray(presence), wrapArray(preppi), wrapArray(cindy), wrapArrayNumber(pvalue),
+				wrapArrayNumber(nes));
 	}
 
-	public static native void createInstance(String containerId, JsArrayString alteration, JsArrayString nes)/*-{
-		$wnd.$citrus_diagram.create(containerId, alteration, nes);
+	public static native void createInstance(String containerId, JsArrayString alteration, JsArrayString samples,
+			JsArrayString presence, JsArrayInteger preppi, JsArrayInteger cindy, JsArrayNumber pvalue,
+			JsArrayNumber nes)/*-{
+		$wnd.$citrus_diagram.create(containerId, alteration, samples, presence, preppi, cindy, pvalue, nes);
 	}-*/;
+
+	public static JsArrayNumber wrapArrayNumber(String[] srcArray) {
+		JsArrayNumber result = JavaScriptObject.createArray().cast();
+		for (int i = 0; i < srcArray.length; i++) {
+			result.set(i, Double.parseDouble(srcArray[i]) );
+		}
+		return result;
+	}
+
+	public static JsArrayInteger wrapArray(int[] srcArray) {
+		JsArrayInteger result = JavaScriptObject.createArray().cast();
+		for (int i = 0; i < srcArray.length; i++) {
+			result.set(i, srcArray[i]);
+		}
+		return result;
+	}
 
 	public static JsArrayString wrapArray(String[] srcArray) {
 		JsArrayString result = JavaScriptObject.createArray().cast();
