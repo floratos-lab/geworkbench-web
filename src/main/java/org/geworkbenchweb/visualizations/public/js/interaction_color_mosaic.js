780ee08e465412f1b1d3d13fcefda3549458bec8
Zhou Ji <zj2122@columbia.edu>
2016-03-11 15:34:44 -0500

    mantis 4036 (font color); add link-out for gene symbol

--- src/main/java/org/geworkbenchweb/visualizations/public/js/interaction_color_mosaic.js
+++ src/main/java/org/geworkbenchweb/visualizations/public/js/interaction_color_mosaic.js
@@ -18,13 +18,17 @@ $interaction_color_mosaic.create = function(id, interactome, gene_symbol,
 	$('#color-mosaic').append(header);
 	for (var i = 0; i < gene_symbol.length; i++) {
 		$('#color-mosaic').append('<tr></tr>');
-		var row = '<td width=100 height=10>' + gene_symbol[i] + '</td>';
+		var row = "<td width=100 height=10><a href='http://www.genecards.org/cgi-bin/carddisp.pl?gene="
+				+ gene_symbol[i]
+				+ "' target='_blank'>"
+				+ gene_symbol[i]
+				+ '</a></td>';
 		for (var j = 0; j < interactome.length; j++) {
 			var index = i*interactome.length + j;
 			var p_formatted = '';
 			var p = parseFloat(p_value[index]);
 			if(!isNaN(p)) p_formatted = p.toFixed(3);
-			row += '<td width=100 height=10 bgcolor=' + color[index] + '>' + p_formatted + '</td>';
+			row += '<td width=100 height=10 bgcolor=' + color[index] + " style='color:white'>" + p_formatted + '</td>';
 		}
 		$('#color-mosaic').find('tr').eq(i + 1).append(row);
 	}
