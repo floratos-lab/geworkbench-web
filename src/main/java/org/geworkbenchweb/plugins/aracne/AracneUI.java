35c0f6c3a448c92ae8abae6ce96319ee3df152fa
Zhou Ji <zhou.ji@yahoo.com>
2023-07-28 12:21:14 -0400

    make bootstrap the dedault

--- src/main/java/org/geworkbenchweb/plugins/aracne/AracneUI.java
+++ src/main/java/org/geworkbenchweb/plugins/aracne/AracneUI.java
@@ -215,6 +215,7 @@ public class AracneUI extends VerticalLayout implements AnalysisUI {
 			}
 
 		});
+		bootStrapNumber.setValue(true);
 
 		mergeProbeSets.setCaption("Merge multiple probesets" + QUESTION_MARK);
 		mergeProbeSets.setDescription(
