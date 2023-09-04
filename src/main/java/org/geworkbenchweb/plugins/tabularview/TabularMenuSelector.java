78ad61a6ebc9c8d7b0404524e4c230e1208d5487
Zhou Ji <zj2122@columbia.edu>
2023-09-03 20:45:54 -0400

    code cleaning-up

--- src/main/java/org/geworkbenchweb/plugins/tabularview/TabularMenuSelector.java
+++ src/main/java/org/geworkbenchweb/plugins/tabularview/TabularMenuSelector.java
@@ -19,272 +19,215 @@ import com.vaadin.ui.Window;
 import de.steinwedel.vaadin.MessageBox;
 import de.steinwedel.vaadin.MessageBox.ButtonType;
 
- 
 public class TabularMenuSelector extends TableMenuSelector {
-	
-	 
+
+	public TabularMenuSelector(Tabular tabular, String tabularName) {
+		super(tabular, tabularName);
+	}
+
 	private static final long serialVersionUID = -2811557010017084202L;
 
-	public TabularMenuSelector(Tabular tabular, String name)
-	{
-		super(tabular, name);
-	}
-	
 	@Override
-	protected Map<String, Command> createDisplayPreferenceItems()
-	{
+	protected Map<String, Command> createDisplayPreferenceItems() {
 		Map<String, Command> map = new HashMap<String, Command>();
 		map.put("Gene Symbol/Marker ID", new Command() {
 
-					private static final long serialVersionUID = 1L;
+			private static final long serialVersionUID = 1L;
 
-					@SuppressWarnings("deprecation")
+			@SuppressWarnings("deprecation")
+			@Override
+			public void menuSelected(MenuItem selectedItem) {
+				final Window displayPrefWindow = new Window();
+				displayPrefWindow.setModal(true);
+				displayPrefWindow.setClosable(true);
+				((AbstractOrderedLayout) displayPrefWindow.getLayout())
+						.setSpacing(true);
+				displayPrefWindow.setWidth("300px");
+				displayPrefWindow.setHeight("200px");
+				displayPrefWindow.setResizable(false);
+				displayPrefWindow.setCaption("Display Preference");
+				displayPrefWindow.setImmediate(true);
+
+				final OptionGroup og;
+				og = new OptionGroup();
+				og.setImmediate(true);
+				og.addItem(Constants.MarkerDisplayControl.marker.ordinal());
+				og.addItem(Constants.MarkerDisplayControl.gene_symbol.ordinal());
+				og.addItem(Constants.MarkerDisplayControl.both.ordinal());
+				og.setItemCaption(Constants.MarkerDisplayControl.marker.ordinal(), "Marker ID");
+				og.setItemCaption(Constants.MarkerDisplayControl.gene_symbol.ordinal(), "Gene Symbol");
+				og.setItemCaption(Constants.MarkerDisplayControl.both.ordinal(), "Both");
+				og.select(((TabularViewUI) parent).getTabViewPreferences().getMarkerDisplayControl());
+
+				final Window mainWindow = getApplication().getMainWindow();
+
+				Button submit = new Button("Submit", new Button.ClickListener() {
+
+					private static final long serialVersionUID = -4799561372701936132L;
+
 					@Override
-					public void menuSelected(MenuItem selectedItem) {
-						final Window displayPrefWindow = new Window();
-						displayPrefWindow.setModal(true);
-						displayPrefWindow.setClosable(true);
-						((AbstractOrderedLayout) displayPrefWindow.getLayout())
-								.setSpacing(true);
-						displayPrefWindow.setWidth("300px");
-						displayPrefWindow.setHeight("200px");
-						displayPrefWindow.setResizable(false);
-						displayPrefWindow.setCaption("Display Preference");
-						displayPrefWindow.setImmediate(true);
+					public void buttonClick(ClickEvent event) {
+						try {
 
-						final OptionGroup og;
-						og = new OptionGroup();
-						og.setImmediate(true);
-						og.addItem(Constants.MarkerDisplayControl.marker
-								.ordinal());
-						og.addItem(Constants.MarkerDisplayControl.gene_symbol
-								.ordinal());
-						og.addItem(Constants.MarkerDisplayControl.both
-								.ordinal());
-						og.setItemCaption(
-								Constants.MarkerDisplayControl.marker.ordinal(),
-								"Marker ID");
-						og.setItemCaption(
-								Constants.MarkerDisplayControl.gene_symbol
-										.ordinal(), "Gene Symbol");
-						og.setItemCaption(
-								Constants.MarkerDisplayControl.both.ordinal(),
-								"Both");
+							Object value = og.getValue();
+							Preference p = PreferenceOperations.getData(Constants.MARKER_DISPLAY_CONTROL,
+									parent.getUserId());
+							if (p != null)
+								PreferenceOperations.setValue(value, p);
+							else
+								PreferenceOperations.storeData(value, Integer.class.getName(),
+										Constants.MARKER_DISPLAY_CONTROL, null, parent.getUserId());
 
-						og.select(((TabularViewUI)parent).getTabViewPreferences().getMarkerDisplayControl());
+							parent.resetDataSource();
 
-						final Window mainWindow = getApplication()
-								.getMainWindow();
+							mainWindow.removeWindow(displayPrefWindow);
 
-						Button submit = new Button("Submit",
-								new Button.ClickListener() {
-
-									private static final long serialVersionUID = -4799561372701936132L;
-
-									@Override
-									public void buttonClick(ClickEvent event) {
-										try {
-
-											Object value = og.getValue();
-											Preference p = PreferenceOperations
-													.getData(
-															Constants.MARKER_DISPLAY_CONTROL,
-															parent.getUserId());
-											if (p != null)
-												PreferenceOperations.setValue(
-														value, p);
-											else
-												PreferenceOperations.storeData(
-														value,
-														Integer.class.getName(),
-														Constants.MARKER_DISPLAY_CONTROL,
-														null, parent.getUserId());
-
-											parent.resetDataSource();				 
-													 
-											mainWindow
-													.removeWindow(displayPrefWindow);
-									 
-										} catch (Exception e) {
-											e.printStackTrace();
-										}
-									}
-								});
-						submit.setClickShortcut(KeyCode.ENTER);
-						displayPrefWindow.addComponent(og);
-						displayPrefWindow.addComponent(submit);
-						mainWindow.addWindow(displayPrefWindow);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
 					}
 				});
+				submit.setClickShortcut(KeyCode.ENTER);
+				displayPrefWindow.addComponent(og);
+				displayPrefWindow.addComponent(submit);
+				mainWindow.addWindow(displayPrefWindow);
+			}
+		});
 
 		map.put("Annotations", new Command() {
 
-					private static final long serialVersionUID = 1L;
+			private static final long serialVersionUID = 1L;
 
-					@SuppressWarnings("deprecation")
+			@SuppressWarnings("deprecation")
+			@Override
+			public void menuSelected(MenuItem selectedItem) {
+				final Window displayPrefWindow = new Window();
+				displayPrefWindow.setModal(true);
+				displayPrefWindow.setClosable(true);
+				((AbstractOrderedLayout) displayPrefWindow.getLayout())
+						.setSpacing(true);
+				displayPrefWindow.setWidth("300px");
+				displayPrefWindow.setHeight("150px");
+				displayPrefWindow.setResizable(false);
+				displayPrefWindow.setCaption("Display Preference");
+				displayPrefWindow.setImmediate(true);
+
+				final OptionGroup og;
+				og = new OptionGroup();
+				og.setImmediate(true);
+				og.addItem(Constants.AnnotationDisplayControl.on.ordinal());
+				og.addItem(Constants.AnnotationDisplayControl.off.ordinal());
+				og.setItemCaption(Constants.AnnotationDisplayControl.on.ordinal(), "Show annotations");
+				og.setItemCaption(Constants.AnnotationDisplayControl.off.ordinal(), "Hide annotations");
+
+				og.select(((TabularViewUI) parent).getTabViewPreferences().getAnnotationDisplayControl());
+
+				final Window mainWindow = getApplication().getMainWindow();
+
+				Button submit = new Button("Submit", new Button.ClickListener() {
+
+					private static final long serialVersionUID = -4799561372701936132L;
+
 					@Override
-					public void menuSelected(MenuItem selectedItem) {
-						final Window displayPrefWindow = new Window();
-						displayPrefWindow.setModal(true);
-						displayPrefWindow.setClosable(true);
-						((AbstractOrderedLayout) displayPrefWindow.getLayout())
-								.setSpacing(true);
-						displayPrefWindow.setWidth("300px");
-						displayPrefWindow.setHeight("150px");
-						displayPrefWindow.setResizable(false);
-						displayPrefWindow.setCaption("Display Preference");
-						displayPrefWindow.setImmediate(true);
+					public void buttonClick(ClickEvent event) {
+						try {
+							Object value = og.getValue();
+							Preference p = PreferenceOperations.getData(Constants.ANNOTATION_DISPLAY_CONTROL,
+									parent.getUserId());
+							if (p != null)
+								PreferenceOperations.setValue(value, p);
+							else
+								PreferenceOperations.storeData(value, Integer.class.getName(),
+										Constants.ANNOTATION_DISPLAY_CONTROL, null, parent.getUserId());
 
-						final OptionGroup og;
-						og = new OptionGroup();
-						og.setImmediate(true);
-						og.addItem(Constants.AnnotationDisplayControl.on
-								.ordinal());
-						og.addItem(Constants.AnnotationDisplayControl.off
-								.ordinal());
-						og.setItemCaption(
-								Constants.AnnotationDisplayControl.on.ordinal(),
-								"Show annotations");
-						og.setItemCaption(
-								Constants.AnnotationDisplayControl.off
-										.ordinal(), "Hide annotations");
+							parent.resetDataSource();
 
-						og.select(((TabularViewUI)parent).getTabViewPreferences()
-								.getAnnotationDisplayControl());
+							mainWindow.removeWindow(displayPrefWindow);
 
-						final Window mainWindow = getApplication()
-								.getMainWindow();
-
-						Button submit = new Button("Submit",
-								new Button.ClickListener() {
-
-									private static final long serialVersionUID = -4799561372701936132L;
-
-									@Override
-									public void buttonClick(ClickEvent event) {
-										try {
-
-											Object value = og.getValue();
-											Preference p = PreferenceOperations
-													.getData(
-															Constants.ANNOTATION_DISPLAY_CONTROL,
-															parent.getUserId());
-											if (p != null)
-												PreferenceOperations.setValue(
-														value, p);
-											else
-												PreferenceOperations.storeData(
-														value,
-														Integer.class.getName(),
-														Constants.ANNOTATION_DISPLAY_CONTROL,
-														null, parent.getUserId());
-
-											parent.resetDataSource();		 
-															 
-											mainWindow
-													.removeWindow(displayPrefWindow);
-										    
-										} catch (Exception e) {
-											e.printStackTrace();
-										}
-									}
-								});
-						submit.setClickShortcut(KeyCode.ENTER);
-						displayPrefWindow.addComponent(og);
-						displayPrefWindow.addComponent(submit);
-						mainWindow.addWindow(displayPrefWindow);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
 					}
 				});
+				submit.setClickShortcut(KeyCode.ENTER);
+				displayPrefWindow.addComponent(og);
+				displayPrefWindow.addComponent(submit);
+				mainWindow.addWindow(displayPrefWindow);
+			}
+		});
 
 		map.put("Precision", new Command() {
 
-					private static final long serialVersionUID = 1L;
+			private static final long serialVersionUID = 1L;
 
-					@SuppressWarnings("deprecation")
-					@Override
-					public void menuSelected(MenuItem selectedItem) {
-						final Window displayPrefWindow = new Window();
-						displayPrefWindow.setModal(true);
-						displayPrefWindow.setClosable(true);
-						((AbstractOrderedLayout) displayPrefWindow.getLayout())
-								.setSpacing(true);
-						displayPrefWindow.setWidth("300px");
-						displayPrefWindow.setHeight("200px");
-						displayPrefWindow.setResizable(false);
-						displayPrefWindow.setCaption("Display Preference");
-						displayPrefWindow.setImmediate(true);
+			@SuppressWarnings("deprecation")
+			@Override
+			public void menuSelected(MenuItem selectedItem) {
+				final Window displayPrefWindow = new Window();
+				displayPrefWindow.setModal(true);
+				displayPrefWindow.setClosable(true);
+				((AbstractOrderedLayout) displayPrefWindow.getLayout()).setSpacing(true);
+				displayPrefWindow.setWidth("300px");
+				displayPrefWindow.setHeight("200px");
+				displayPrefWindow.setResizable(false);
+				displayPrefWindow.setCaption("Display Preference");
+				displayPrefWindow.setImmediate(true);
 
-						final TextField precision;
-						precision = new TextField();
-						precision.setCaption("Precision");
-						precision.setValue(((TabularViewUI)parent).getTabViewPreferences()
-								.getNumberPrecisionControl());
+				final TextField precision;
+				precision = new TextField();
+				precision.setCaption("Precision");
+				precision.setValue(((TabularViewUI) parent).getTabViewPreferences().getNumberPrecisionControl());
 
-						final Window mainWindow = getApplication()
-								.getMainWindow();
+				final Window mainWindow = getApplication().getMainWindow();
 
-						Button submit = new Button("Submit",
-								new Button.ClickListener() {
+				Button submit = new Button("Submit",
+						new Button.ClickListener() {
 
-									private static final long serialVersionUID = -4799561372701936132L;
+							private static final long serialVersionUID = -4799561372701936132L;
 
-									@Override
-									public void buttonClick(ClickEvent event) {
-										try {
+							@Override
+							public void buttonClick(ClickEvent event) {
+								try {
+									Object value = precision.getValue();
+									Preference p = PreferenceOperations.getData(Constants.NUMBER_PRECISION_CONTROL,
+											parent.getUserId());
+									if (p != null)
+										PreferenceOperations.setValue(Integer.valueOf(value.toString().trim()), p);
+									else
+										PreferenceOperations.storeData(Integer.valueOf(value.toString().trim()),
+												Integer.class.getName(), Constants.NUMBER_PRECISION_CONTROL, null,
+												parent.getUserId());
 
-											Object value = precision.getValue();
-											Preference p = PreferenceOperations
-													.getData(
-															Constants.NUMBER_PRECISION_CONTROL,
-															parent.getUserId());
-											if (p != null)
-												PreferenceOperations.setValue(
-														new Integer(value
-																.toString()
-																.trim()), p);
-											else
-												PreferenceOperations
-														.storeData(
-																new Integer(
-																		value.toString()
-																				.trim()),
-																Integer.class
-																		.getName(),
-																Constants.NUMBER_PRECISION_CONTROL,
-																null, parent.getUserId());
+									((TabularViewUI) parent)
+											.setPrecisonNumber(Integer.valueOf(value.toString().trim()));
+									parent.resetDataSource();
+									((TabularViewUI) parent).getTabViewPreferences()
+											.setNumberPrecisionControl(Integer.valueOf(value.toString().trim()));
+									mainWindow
+											.removeWindow(displayPrefWindow);
 
-											((TabularViewUI)parent).setPrecisonNumber( new Integer(value.toString().trim()));											
-											parent.resetDataSource();
-											((TabularViewUI)parent).getTabViewPreferences().setNumberPrecisionControl( new Integer(value.toString().trim()));
-											mainWindow
-													.removeWindow(displayPrefWindow);
-										 
-										} catch (NumberFormatException nfe) {
-											MessageBox mb = new MessageBox(
-													getWindow(),
-													"Warning",
-													MessageBox.Icon.WARN,
-													"Please enter a number. ",
-													new MessageBox.ButtonConfig(
-															ButtonType.OK, "Ok"));
-											mb.show();
+								} catch (NumberFormatException nfe) {
+									MessageBox mb = new MessageBox(getWindow(), "Warning", MessageBox.Icon.WARN,
+											"Please enter a number. ",
+											new MessageBox.ButtonConfig(ButtonType.OK, "Ok"));
+									mb.show();
 
-										} catch (Exception e) {
-											e.printStackTrace();
-										}
-									}
-								});
-						submit.setClickShortcut(KeyCode.ENTER);
-						displayPrefWindow.addComponent(precision);
-						displayPrefWindow.addComponent(submit);
-						mainWindow.addWindow(displayPrefWindow);
-					}
-				});
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							}
+						});
+				submit.setClickShortcut(KeyCode.ENTER);
+				displayPrefWindow.addComponent(precision);
+				displayPrefWindow.addComponent(submit);
+				mainWindow.addWindow(displayPrefWindow);
+			}
+		});
 		return map;
 	}
-	
+
 	@Override
 	protected Window createFilterWindow() {
-		return new FilterWindow(parent);			 
-	} 
+		return new FilterWindow(parent);
+	}
 }
