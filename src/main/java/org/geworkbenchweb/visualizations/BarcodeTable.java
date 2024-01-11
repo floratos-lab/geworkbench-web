6f83e3ba806f0bca1af04663d50ad740cc88515a
Zhou Ji <zj2122@columbia.edu>
2024-01-11 14:33:15 -0500

    minimal change of BarcodeTable for vaadin 7

--- src/main/java/org/geworkbenchweb/visualizations/BarcodeTable.java
+++ src/main/java/org/geworkbenchweb/visualizations/BarcodeTable.java
@@ -1,9 +1,9 @@
 package org.geworkbenchweb.visualizations;
- 
-import java.util.Map; 
+
+import java.util.Map;
 import java.util.List;
 import com.vaadin.server.PaintException;
-import com.vaadin.server.PaintTarget; 
+import com.vaadin.server.PaintTarget;
 import com.vaadin.ui.AbstractComponent;
 
 import flexjson.JSONSerializer;
@@ -12,54 +12,36 @@ import flexjson.JSONSerializer;
  * Server side component for the VCytoscape widget.
  */
 public class BarcodeTable extends AbstractComponent {
-	 
-	private static final long serialVersionUID = -2119963715440692055L;
-	 
-	private String[] columnNames = {"Regulator", "Bar Graph", "PValue","Activity", "Expression", "Exp. Rank"};
-	private List<Regulator>  regulators;
-	private Map<String, List<Barcode>>   barcodeMap;
-	private int barHeight;
-	//private boolean exportImage = false;	 
-	 
-	public BarcodeTable(){};
-	public BarcodeTable(List<Regulator> regulators, Map<String, 
-			List<Barcode>> barcodeMap, int barHeight) {
-		
-	       this.regulators =  regulators;
-	       this.barcodeMap = barcodeMap;
-	       this.barHeight = barHeight;
-	       //this.exportImage = exportImage;
-	}
-	 
 
-	 
-	@Override
+	private static final long serialVersionUID = -2119963715440692055L;
+
+	private String[] columnNames = { "Regulator", "Bar Graph", "PValue", "Activity", "Expression", "Exp. Rank" };
+	private List<Regulator> regulators;
+	private Map<String, List<Barcode>> barcodeMap;
+	private int barHeight;
+	// private boolean exportImage = false;
+
+	public BarcodeTable() {
+	};
+
+	public BarcodeTable(List<Regulator> regulators, Map<String, List<Barcode>> barcodeMap, int barHeight) {
+
+		this.regulators = regulators;
+		this.barcodeMap = barcodeMap;
+		this.barHeight = barHeight;
+		// this.exportImage = exportImage;
+	}
+
+	// FIXME this should be replaced by vaadin 7 communication mechanism
 	public void paintContent(PaintTarget target) throws PaintException {
-		super.paintContent(target);
-	 
-		
+
 		JSONSerializer jsonSerializer = new JSONSerializer().exclude("*.class");
-		
-        target.addAttribute("columnNames",jsonSerializer.deepSerialize(columnNames));
+
+		target.addAttribute("columnNames", jsonSerializer.deepSerialize(columnNames));
 		target.addAttribute("regulators", jsonSerializer.deepSerialize(regulators));
 		target.addAttribute("barcodeMap", jsonSerializer.deepSerialize(barcodeMap));
 		target.addAttribute("barHeight", barHeight);
-		//target.addAttribute("exportImage", exportImage);
-		
-	}
+		// target.addAttribute("exportImage", exportImage);
 
-	/**
-	 * Receive and handle events and other variable changes from the client.
-	 * 
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void changeVariables(Object source, Map<String, Object> variables) {
-		super.changeVariables(source, variables);
-		
-	 
-	} 
-	
-	 
-	
+	}
 }
