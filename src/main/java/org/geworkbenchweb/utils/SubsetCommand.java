ccfb94d77356729b2a902b2f4fcb9ba1c86dff94
Zhou Ji <zj2122@columbia.edu>
2024-01-12 13:08:20 -0500

    clean up utils.SubsetCommand for vaadin 7

--- src/main/java/org/geworkbenchweb/utils/SubsetCommand.java
+++ src/main/java/org/geworkbenchweb/utils/SubsetCommand.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.geworkbenchweb.utils;
 
 import java.util.List;
@@ -15,30 +12,36 @@ import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.MenuBar.Command;
 import com.vaadin.ui.MenuBar.MenuItem;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.Tree;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
 /**
  * 
- * Command to create subset. 
- * 
- * @author zji
- * 
+ * Command to create subset.
  */
-/* TODO This is based on a class in hierarchical clustering package. That one probably would be better to be merged with this. */
+/*
+ * TODO This is based on a class in hierarchical clustering package. That one
+ * probably would be better to be merged with this.
+ */
 public abstract class SubsetCommand implements Command {
-	
-	protected abstract List<String> getItems(); /* this depends how the items are got, 
-	considering the items are usually not set when this class is instantiated, but when the command is run ('menuSelected'). */
+
+	protected abstract List<String> getItems(); /*
+												 * this depends how the items are got,
+												 * considering the items are usually not set when this class is
+												 * instantiated, but when the command is run ('menuSelected').
+												 */
 
 	private static final long serialVersionUID = 1348332795447686854L;
 	private static Log log = LogFactory.getLog(SubsetCommand.class);
-	
-	public enum SetType {MARKER, MICROARRAY};
+
+	public enum SetType {
+		MARKER, MICROARRAY
+	};
 
 	final private String caption;
 	final private Component parent;
@@ -50,7 +53,6 @@ public abstract class SubsetCommand implements Command {
 		this.parent = parent;
 		this.setType = setType;
 		this.parentId = parentId;
-		
 	}
 
 	@Override
@@ -69,26 +71,27 @@ public abstract class SubsetCommand implements Command {
 		setName.setInputPrompt("Please enter set name");
 		setName.setImmediate(true);
 
-		if(parent==null) return; // parent should never be null
-		final Window mainWindow = parent.getApplication().getMainWindow();
-		
+		if (parent == null)
+			return; // parent should never be null
+		final UI mainWindow = UI.getCurrent();
+
 		Button submit = new Button("Submit", new Button.ClickListener() {
 
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			public void buttonClick(ClickEvent event) {
-				
+
 				UMainLayout mainLayout = null;
-				ComponentContainer content = mainWindow.getContent();
-				if(content instanceof UMainLayout) {
-					mainLayout = (UMainLayout)content;
+				Component content = mainWindow.getContent();
+				if (content instanceof UMainLayout) {
+					mainLayout = (UMainLayout) content;
 				} else {
 					log.error("unable to get UMainLayout");
 					return;
 				}
 				SetViewLayout setViewLayout = mainLayout.getSetViewLayout();
-				
+
 				String newSetName = (String) setName.getValue();
 
 				try {
@@ -97,48 +100,52 @@ public abstract class SubsetCommand implements Command {
 					Tree tree = null;
 
 					List<String> items = getItems();
-					if(setName.getValue() != null) {
+					if (setName.getValue() != null) {
 
-						// TODO why are marker and arrays treated differently? 
+						// TODO why are marker and arrays treated differently?
 						// TODO use List instead of ArrayList when possible
-						if(setType == SetType.MARKER) {
+						if (setType == SetType.MARKER) {
 							subSetId = SubSetOperations.storeMarkerSetInCurrentContext(items, newSetName, parentId);
 							parentSet = "MarkerSets";
-							if(setViewLayout!=null) {
+							if (setViewLayout != null) {
 								tree = setViewLayout.getMarkerSetTree();
 							}
 						} else { // MICRORRAY
 							subSetId = SubSetOperations.storeArraySetInCurrentContext(items, newSetName, parentId);
 							parentSet = "arraySets";
-							if(setViewLayout!=null) {
+							if (setViewLayout != null) {
 								tree = setViewLayout.getArraySetTree();
 							}
 						}
-						mainWindow.removeWindow(nameWindow);
+						UI.getCurrent().removeWindow(nameWindow);
 					}
-					
-					if(tree!=null) { /* set view instead of workspace view */
+
+					if (tree != null) { /* set view instead of workspace view */
 						tree.addItem(subSetId);
 						tree.getContainerProperty(subSetId, SetViewLayout.SUBSET_NAME).setValue(newSetName);
-						tree.getContainerProperty(subSetId, SetViewLayout.SET_DISPLAY_NAME).setValue(newSetName + " [" + items.size() + "]");
+						tree.getContainerProperty(subSetId, SetViewLayout.SET_DISPLAY_NAME)
+								.setValue(newSetName + " [" + items.size() + "]");
 						tree.setParent(subSetId, parentSet);
 						tree.setChildrenAllowed(subSetId, true);
-						for(int j=0; j<items.size(); j++) {
-							String itemLabel = items.get(j); 
-							tree.addItem(itemLabel+subSetId);
-							tree.getContainerProperty(itemLabel+subSetId, SetViewLayout.SET_DISPLAY_NAME).setValue(itemLabel);
-							tree.setParent(itemLabel+subSetId, subSetId);
-							tree.setChildrenAllowed(itemLabel+subSetId, false);
+						for (int j = 0; j < items.size(); j++) {
+							String itemLabel = items.get(j);
+							tree.addItem(itemLabel + subSetId);
+							tree.getContainerProperty(itemLabel + subSetId, SetViewLayout.SET_DISPLAY_NAME)
+									.setValue(itemLabel);
+							tree.setParent(itemLabel + subSetId, subSetId);
+							tree.setChildrenAllowed(itemLabel + subSetId, false);
 						}
 					}
-				} catch(Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 		submit.setClickShortcut(KeyCode.ENTER);
-		nameWindow.addComponent(setName);
-		nameWindow.addComponent(submit);
+		VerticalLayout layout = new VerticalLayout();
+		layout.addComponent(setName);
+		layout.addComponent(submit);
+		nameWindow.setContent(layout);
 		mainWindow.addWindow(nameWindow);
 	}
 
