c52a036c4c9a27983ac17d5fad83e09fc65c7fc2
Zhou Ji <zj2122@columbia.edu>
2024-01-12 14:29:47 -0500

    minimal change of Cytoscape for vaadin 7

--- src/main/java/org/geworkbenchweb/visualizations/Cytoscape.java
+++ src/main/java/org/geworkbenchweb/visualizations/Cytoscape.java
@@ -13,41 +13,41 @@ public class Cytoscape extends AbstractComponent {
 
 	private String[] nodes;
 	private String[] edges;
-	
+
 	private String layoutName = "concentric"; // default
-	
-	@Override
+
+	// FIXME this should be replaced by vaadin 7 communication mechanism
 	public void paintContent(PaintTarget target) throws PaintException {
-		super.paintContent(target);
-		
+
 		target.addAttribute("layoutName", layoutName);
 
 		target.addVariable(this, "nodes", getNodes());
 		target.addVariable(this, "edges", getEdges());
 
-		if(colors!=null) target.addVariable(this, "colors", colors);
+		if (colors != null)
+			target.addVariable(this, "colors", colors);
 	}
 
 	public void setNodes(String[] nodes) {
-        this.nodes = nodes;
-        requestRepaint();
+		this.nodes = nodes;
+		requestRepaint();
 	}
 
 	public String[] getNodes() {
-        
+
 		return nodes;
-	
+
 	}
-	
+
 	public void setEdges(String[] edges) {
-        this.edges = edges;
-        requestRepaint();
+		this.edges = edges;
+		requestRepaint();
 	}
 
 	public String[] getEdges() {
-        
+
 		return edges;
-	
+
 	}
 
 	public void setLayout(String layoutName) {
@@ -55,9 +55,10 @@ public class Cytoscape extends AbstractComponent {
 	}
 
 	private String[] colors = null;
+
 	public void setColor(String[] colors) {
 		this.colors = colors;
 		requestRepaint();
 	}
-	
+
 }
