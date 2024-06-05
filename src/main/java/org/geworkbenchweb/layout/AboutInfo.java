3fcb2148e9a3f584b89dc275a25aa82711b3c942
Zhou Ji <zj2122@columbia.edu>
2024-06-05 13:05:04 -0400

    minor cleaning-up

--- src/main/java/org/geworkbenchweb/layout/AboutInfo.java
+++ src/main/java/org/geworkbenchweb/layout/AboutInfo.java
@@ -1,34 +1,28 @@
-/**
- * 
- */
 package org.geworkbenchweb.layout;
 
 import org.geworkbenchweb.GeworkbenchRoot;
 
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
-/**
- * @author zji
- * 
- */
 public class AboutInfo extends VerticalLayout {
 
 	private static final long serialVersionUID = -93556097597644299L;
 
 	@Override
 	public void attach() {
-		
+
 		String releaseNumber = GeworkbenchRoot.getAppProperty("release.number");
 		String releaseDate = GeworkbenchRoot.getAppProperty("release.date");
 		String buildTimestamp = GeworkbenchRoot.getAppProperty("build.timestamp");
 		String buildVersion = GeworkbenchRoot.getAppProperty("build.version");
-		
+
 		String text = String.format("<b>Current Release</b><ul><li>Release number: %s<br/><li>Release Date: %s"
-				+"<li>Build Timestamp: %s<li>Build version: %s"
-				+"</ul><b>Public project web site: </b><a href='http://www.geworkbench.org' target='_blank'>http://www.geworkbench.org</a>",
+				+ "<li>Build Timestamp: %s<li>Build version: %s"
+				+ "</ul><b>Public project web site: </b><a href='http://www.geworkbench.org' target='_blank'>http://www.geworkbench.org</a>",
 				releaseNumber, releaseDate, buildTimestamp, buildVersion);
-		Label label = new Label(text, Label.CONTENT_XHTML);
+		Label label = new Label(text, ContentMode.HTML);
 		addComponent(label);
 		this.setMargin(true);
 	}
